ef3a3f26cd79ddeb0539af7c4df2fe519933f5a1
Son Ho <hosonmarc@gmail.com>
2021-10-27 11:39:24 +0200

    Implement build_cfg_no_back_edges

--- charon/src/im_to_cfim.rs
+++ charon/src/im_to_cfim.rs
@@ -9,6 +9,7 @@ use crate::translate_functions_to_im::FunTransContext;
 use crate::types::*;
 use crate::values::*;
 use hashlink::linked_hash_map::LinkedHashMap;
+use im;
 use im::{OrdMap, OrdSet, Vector};
 use macros::{EnumAsGetters, EnumIsA, VariantName};
 use petgraph::algo::dominators::simple_fast;
@@ -125,16 +126,86 @@ fn build_cfg_edges(
     }
 }
 
+/// This structure contains the CFG for a function body, where all the backward
+/// edges have been removed.
+struct CfgNoBackEdges {
+    pub cfg: Cfg,
+    /// We consider the destination of the backward edges to be loop entries and
+    /// store them here.
+    pub loop_entries: HashSet<src::BlockId::Id>,
+    /// The blocks whose terminators are a switch are stored here.
+    pub switch_blocks: HashSet<src::BlockId::Id>,
+}
+
+fn build_cfg_no_back_edges(body: &src::Body) -> CfgNoBackEdges {
+    let mut cfg = CfgNoBackEdges {
+        cfg: Cfg::new(),
+        loop_entries: HashSet::new(),
+        switch_blocks: HashSet::new(),
+    };
+
+    // Add the nodes
+    for block_id in body.blocks.iter_indices() {
+        cfg.cfg.add_node(block_id);
+    }
+
+    // Add the edges
+    let previous = im::HashSet::new();
+    build_cfg_no_back_edges_edges(&mut cfg, &previous, body, src::BlockId::ZERO);
+
+    cfg
+}
+
+fn block_is_switch(body: &src::Body, block_id: src::BlockId::Id) -> bool {
+    let block = body.blocks.get(block_id).unwrap();
+    block.terminator.is_switch()
+}
+
+fn build_cfg_no_back_edges_edges(
+    cfg: &mut CfgNoBackEdges,
+    previous: &im::HashSet<src::BlockId::Id>,
+    body: &src::Body,
+    block_id: src::BlockId::Id,
+) {
+    // Insert the current block in the set of previous blocks
+    let mut previous = previous.clone();
+    previous.insert(block_id);
+
+    // Check if it is a switch
+    if block_is_switch(body, block_id) {
+        cfg.switch_blocks.insert(block_id);
+    }
+
+    // Retrieve the block targets
+    let targets = get_block_targets(body, block_id);
+
+    // Add edges for all the targets and explore them, if they are not predecessors
+    for tgt in &targets {
+        if previous.contains(tgt) {
+            // This is a backward edge
+            cfg.loop_entries.insert(*tgt);
+        } else {
+            // Not a backward edge: insert the edge and explore
+            cfg.cfg.add_edge(block_id, *tgt, ());
+            build_cfg_no_back_edges_edges(cfg, &previous, body, *tgt);
+        }
+    }
+}
+
 fn translate_function(im_ctx: &FunTransContext, src_decl_id: DefId::Id) -> Result<tgt::FunDecl> {
     // Retrieve the function declaration
     let src_decl = im_ctx.decls.get(src_decl_id).unwrap();
 
+    // Explore the function body to create the control-flow graph without backward
+    // edges, and identify the loop entries (which are destinations of backward edges).
+    let cfg_no_be = build_cfg_no_back_edges(&src_decl.body);
+
     // Explore the function body to create the control-flow graph
-    let cfg = build_cfg(&src_decl.body);
+    //    let cfg = build_cfg(&src_decl.body);
 
     // Analyze the graph to look for dominators and strongly connected components (loops)
-    let sccs = tarjan_scc(&cfg);
-    let dominators = simple_fast(&cfg, src::BlockId::ZERO);
+    //    let sccs = tarjan_scc(&cfg);
+    //    let dominators = simple_fast(&cfg, src::BlockId::ZERO);
 
     // Find the loops and the conditional branchings
     unimplemented!();
