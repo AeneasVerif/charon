07813aaf8184edfe40ec785a677db43dcf86b3ff
Son Ho <hosonmarc@gmail.com>
2022-02-04 09:12:04 +0100

    Add examples with several (independent) lifetimes in no_nested_borrows.rs

--- charon/tests/no_nested_borrows.rs
+++ charon/tests/no_nested_borrows.rs
@@ -235,3 +235,11 @@ pub fn list_nth<'a, T>(l: &'a mut List<T>, i: u32) -> &'a mut T {
         }
     }
 }
+
+pub fn id_mut_pair1<'a, 'b, T1, T2>(x: &'a mut T1, y: &'b mut T2) -> (&'a mut T1, &'b mut T2) {
+    (x, y)
+}
+
+pub fn id_mut_pair2<'a, 'b, T1, T2>(p: (&'a mut T1, &'b mut T2)) -> (&'a mut T1, &'b mut T2) {
+    p
+}
