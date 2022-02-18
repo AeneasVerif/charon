72a058884513e9027c00a1413e8b9290e6779da4
Jonathan Protzenko <protz@microsoft.com>
2022-02-17 16:32:34 -0800

    Another example

--- charon/tests/src/paper.rs
+++ charon/tests/src/paper.rs
@@ -30,3 +30,45 @@ fn test_choose() {
     assert!(x == 1);
     assert!(y == 0);
 }
+
+// 2.3
+
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil,
+}
+use crate::List::Cons;
+use crate::List::Nil;
+
+fn list_nth_mut<'a, T>(l: &'a mut List<T>, i: u32) -> &'a mut T {
+    match l {
+        Nil => {
+            panic!()
+        }
+        Cons(x, tl) => {
+            if i == 0 {
+                return x;
+            } else {
+                return list_nth_mut(tl, i - 1);
+            }
+        }
+    }
+}
+
+fn sum(l: & List<i32>) -> i32 {
+    match l {
+        Nil => {
+            return 0;
+        }
+        Cons (x, tl) => {
+            return *x + sum(tl);
+        }
+    }
+}
+
+fn test_nth() {
+    let mut l = Cons (1, Box::new(Cons (2, Box::new(Cons (3, Box::new(Nil))))));
+    let x = list_nth_mut(&mut l, 2);
+    *x = *x + 1;
+    assert!(sum(&l) == 7);
+}
