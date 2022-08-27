7a6a57e3fc4a762d945ff198fe0b9564a507d98d
Sacha Ayoun <sachaayoun@gmail.com>
2022-08-27 17:33:19 -0400

    add ml to makefile, check that everything parses correctly
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

---
+++ ml/tests/e2e.ml
@@ -0,0 +1,13 @@
+let folder_name = Sys.argv.(1)
+
+
+let files = Sys.readdir folder_name |> Array.to_seq
+ |> Seq.filter (fun file -> Filename.check_suffix file ".llbc")
+ 
+let () = Seq.iter (fun file ->
+  let file = Filename.concat folder_name file in
+  let ast = Llbc.Of_json.llbc_module_of_json (Yojson.Basic.from_file file) in let () = match ast with
+    | Ok _ -> Printf.printf "%s parsed correctly" file
+    | Error e -> Printf.printf "parsing error: %s" e in
+    Format.printf "\n@?"
+) files
