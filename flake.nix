b8ec154b09a42869e1fd75694d53a4835a5d5426
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-10-18 08:00:00 +0200

    format

--- flake.nix
+++ flake.nix
@@ -28,47 +28,46 @@
         };
         craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
         charon = craneLib.buildPackage { src = ./charon; };
-        tests =
-          let cargoArtifacts = craneLib.buildDepsOnly { src = ./tests; };
-          in craneLib.buildPackage {
-            src = ./tests;
-            inherit cargoArtifacts;
-            buildPhase = ''
-              mkdir -p $out/llbc
+        tests = let cargoArtifacts = craneLib.buildDepsOnly { src = ./tests; };
+        in craneLib.buildPackage {
+          src = ./tests;
+          inherit cargoArtifacts;
+          buildPhase = ''
+            mkdir -p $out/llbc
 
-              CHARON="${charon}/bin/cargo-charon --cargo-no-rust-version --dest $out/llbc"
+            CHARON="${charon}/bin/cargo-charon --cargo-no-rust-version --dest $out/llbc"
 
-              for test in hashmap matches matches_duplicate
-              do
-                $CHARON --crate $test --input src/$test.rs
-              done
+            for test in hashmap matches matches_duplicate
+            do
+              $CHARON --crate $test --input src/$test.rs
+            done
 
-              for test in nested_borrows no_nested_borrows loops paper constants external
-              do
-                $CHARON --no-code-duplication --crate $test --input src/$test.rs
-              done
+            for test in nested_borrows no_nested_borrows loops paper constants external
+            do
+              $CHARON --no-code-duplication --crate $test --input src/$test.rs
+            done
 
-              $CHARON --opaque=hashmap_utils --crate hashmap_main --input src/hashmap_main.rs
-            '';
-            dontInstall = true;
-          };
+            $CHARON --opaque=hashmap_utils --crate hashmap_main --input src/hashmap_main.rs
+          '';
+          dontInstall = true;
+        };
 
         # TODO: for some reason, -Zpolonius doesn't work (the borrow checking fails)
-        tests-polonius =
-          let cargoArtifacts  = craneLib.buildDepsOnly { src = ./tests-polonius; };
-          in craneLib.buildPackage {
-            name = "tests-polonius";
-            src = ./tests-polonius;
-            inherit cargoArtifacts;
-            buildPhase = ''
-              mkdir -p $out/llbc
+        tests-polonius = let
+          cargoArtifacts = craneLib.buildDepsOnly { src = ./tests-polonius; };
+        in craneLib.buildPackage {
+          name = "tests-polonius";
+          src = ./tests-polonius;
+          inherit cargoArtifacts;
+          buildPhase = ''
+            mkdir -p $out/llbc
 
-              CHARON="${charon}/bin/cargo-charon --cargo-no-rust-version --polonius --dest $out/llbc"
+            CHARON="${charon}/bin/cargo-charon --cargo-no-rust-version --polonius --dest $out/llbc"
 
-              $CHARON --opaque=betree_utils --crate betree_main --input src/betree_main.rs
-            '';
-            dontInstall = true;
-          };
+            $CHARON --opaque=betree_utils --crate betree_main --input src/betree_main.rs
+          '';
+          dontInstall = true;
+        };
       in {
         packages = {
           inherit charon;
