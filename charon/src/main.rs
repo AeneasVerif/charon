2384e29ee7ec464c9ef6dc136e32c25514dd5ee1
Son Ho <hosonmarc@gmail.com>
2022-10-18 17:25:02 +0200

    Add some comments

--- charon/src/main.rs
+++ charon/src/main.rs
@@ -15,6 +15,34 @@ use structopt::StructOpt;
 const RUST_VERSION: &'static str = macros::rust_version!();
 
 pub fn main() {
+    // We followed the approach used by [rust-clippy](https://github.com/rust-lang/rust-clippy).
+    //
+    // It works as follows.
+    // In order to query the results of the Rustc compiler, we need to implement
+    // a driver which calls Rustc while giving it some callbacks.
+    // The problem is that finding the proper arguments to call Rustc with can
+    // be difficult. For instance, provided the project we want to analyse with
+    // Charon has already been built (and in particular its dependencies), it is
+    // very difficult to provide the proper `--extern` arguments, indicating
+    // where to find the compiled external dependencies. For instance, even if
+    // we look in the `target` folder, the compiled depedencies are decorated
+    // with a hash, and we don't know where this hash comes from.
+    // Computing those arguments is, however, Cargo's responsability. As a
+    // consequence, we follow Clippy's approach by piggy-backing on Cargo.  We
+    // call Cargo as if we were building the project, but update the environment
+    // variable `RUSTC_WORKSPACE_WRAPPER` so that Cargo calls `charon-driver`
+    // instead of Rustc upon building the target project. More specifically:
+    // Cargo will call Rustc to build the dependencies, *then* will call
+    // charon-driver with the arguments it would have given to Rustc to build
+    // the target project.
+    // Upon being called, charon-driver (see `driver.rs`) will simply call
+    // Rustc with the arguments it was provided (and a few minor modifications).
+    // Also, in order to transmit options from cargo-charon (this file)
+    // to charon-driver (`driver.rs`), we serialize those options and store
+    // them in a specific environment variable, so that charon-driver can
+    // deserialize them later and use them to guide the extraction in the
+    // callbacks.
+
     // Initialize the logger
     logger::initialize_logger();
 
