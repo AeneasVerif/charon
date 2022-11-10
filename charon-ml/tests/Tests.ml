2dbbbd8d2f915d578ce87c689658d73cdd6da8ea
Son Ho <hosonmarc@gmail.com>
2022-11-10 15:22:33 +0100

    Cleanup the Charon-ML tests

--- charon-ml/tests/Tests.ml
+++ charon-ml/tests/Tests.ml
@@ -2,9 +2,14 @@ open Charon
 open Logging
 module EL = Easy_logging.Logging
 
-(* Set the log level *)
+(* Set the log level - we use the environment variable "CHARON_LOG" *)
 let log = main_log
-let () = log#set_level EL.Info
+
+let () =
+  try
+    let _ = Unix.getenv "CHARON_LOG" in
+    log#set_level EL.Debug
+  with Not_found -> log#set_level EL.Info
 
 (* Call the tests *)
-let () = All_tests.Test_Deserialize.run_tests "../../../tests/serialized"
+let () = Test_Deserialize.run_tests "../../../tests/serialized"
