35d7414b62747918abee60a8377f2517668a5f7b
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2022-11-28 16:17:39 +0100

    Simplify example

--- tests/src/traits.rs
+++ tests/src/traits.rs
@@ -1,16 +1,14 @@
-struct Sheep { naked: bool, name: &'static str }
+struct Sheep { naked: bool, id: u32 }
 
 trait Animal {
     // Associated function signature; `Self` refers to the implementor type.
-    fn new(name: &'static str) -> Self;
+    fn new(id: u32) -> Self;
 
-    // Method signatures; these will return a string.
-    fn name(&self) -> &'static str;
-    fn noise(&self) -> &'static str;
+    fn my_id(&self) -> u32;
 
     // Traits can provide default method definitions.
     fn talk(&self) {
-        println!("{} says {}", self.name(), self.noise());
+        println!("I am {}", self.my_id());
     }
 }
 
@@ -22,9 +20,9 @@ impl Sheep {
     fn shear(&mut self) {
         if self.is_naked() {
             // Implementor methods can use the implementor's trait methods.
-            println!("{} is already naked...", self.name());
+            println!("{} is already naked...", self.my_id());
         } else {
-            println!("{} gets a haircut!", self.name);
+            println!("{} gets a haircut!", self.my_id());
 
             self.naked = true;
         }
@@ -34,102 +32,27 @@ impl Sheep {
 // Implement the `Animal` trait for `Sheep`.
 impl Animal for Sheep {
     // `Self` is the implementor type: `Sheep`.
-    fn new(name: &'static str) -> Sheep {
-        Sheep { name: name, naked: false }
+    fn new(id: u32) -> Sheep {
+        Sheep { id: id, naked: false }
     }
 
-    fn name(&self) -> &'static str {
-        self.name
+    fn my_id(&self) -> u32 {
+        self.id
     }
 
-    fn noise(&self) -> &'static str {
-        if self.is_naked() {
-            "baaaaah?"
-        } else {
-            "baaaaah!"
-        }
-    }
-    
     // Default trait methods can be overridden.
     fn talk(&self) {
         // For example, we can add some quiet contemplation.
-        println!("{} pauses briefly... {}", self.name, self.noise());
+        println!("{} pauses briefly...", self.id);
     }
 }
 
 fn main() {
     // Type annotation is necessary in this case.
-    let mut dolly: Sheep = Animal::new("Dolly");
+    let mut dolly: Sheep = Animal::new(23);
     // TODO ^ Try removing the type annotations.
 
     dolly.talk();
     dolly.shear();
     dolly.talk();
-struct Sheep { naked: bool, name: &'static str }
-
-trait Animal {
-    // Associated function signature; `Self` refers to the implementor type.
-    fn new(name: &'static str) -> Self;
-
-    // Method signatures; these will return a string.
-    fn name(&self) -> &'static str;
-    fn noise(&self) -> &'static str;
-
-    // Traits can provide default method definitions.
-    fn talk(&self) {
-        println!("{} says {}", self.name(), self.noise());
-    }
 }
-
-impl Sheep {
-    fn is_naked(&self) -> bool {
-        self.naked
-    }
-
-    fn shear(&mut self) {
-        if self.is_naked() {
-            // Implementor methods can use the implementor's trait methods.
-            println!("{} is already naked...", self.name());
-        } else {
-            println!("{} gets a haircut!", self.name);
-
-            self.naked = true;
-        }
-    }
-}
-
-// Implement the `Animal` trait for `Sheep`.
-impl Animal for Sheep {
-    // `Self` is the implementor type: `Sheep`.
-    fn new(name: &'static str) -> Sheep {
-        Sheep { name: name, naked: false }
-    }
-
-    fn name(&self) -> &'static str {
-        self.name
-    }
-
-    fn noise(&self) -> &'static str {
-        if self.is_naked() {
-            "baaaaah?"
-        } else {
-            "baaaaah!"
-        }
-    }
-    
-    // Default trait methods can be overridden.
-    fn talk(&self) {
-        // For example, we can add some quiet contemplation.
-        println!("{} pauses briefly... {}", self.name, self.noise());
-    }
-}
-
-fn main() {
-    // Type annotation is necessary in this case.
-    let mut dolly: Sheep = Animal::new("Dolly");
-    // TODO ^ Try removing the type annotations.
-
-    dolly.talk();
-    dolly.shear();
-    dolly.talk();
-}}
