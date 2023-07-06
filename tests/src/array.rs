fc20fa31bcbd69bb51865db497d0cd382216455f
Son Ho <hosonmarc@gmail.com>
2023-07-06 17:23:05 +0200

    Fix a small issue with tests/src/array.rs

--- tests/src/array.rs
+++ tests/src/array.rs
@@ -1,7 +1,7 @@
 //! Exercise the translation of arrays
 #![allow(dead_code)]
 
-fn sum2 (s: &[u32], s2: &[u32]) -> u32 {
+fn sum2(s: &[u32], s2: &[u32]) -> u32 {
     let mut sum = 0;
     assert!(s.len() == s2.len());
     let mut i = 0;
@@ -9,17 +9,14 @@ fn sum2 (s: &[u32], s2: &[u32]) -> u32 {
         sum += s[i] + s2[i];
         i += 1;
     }
-    return sum
+    sum
 }
 
-const SZ: usize = 32;
+// TODO: this makes the compilation fail
+// const SZ: usize = 32;
 
-fn f2 () -> u32 {
-    let a: [u32; 2 ] = [ 1, 2 ];
-    let b = [ 0; SZ ];
-    return sum2(&a, &b[16..18])
-}
-
-fn main() {
-    print!("{}", f2());
+fn f2() -> u32 {
+    let a: [u32; 2] = [1, 2];
+    let b = [0; 32];
+    sum2(&a, &b[16..18])
 }
