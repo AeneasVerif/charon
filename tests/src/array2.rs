ad201c372893d94027ad055ab42a4dd8c62e2bf7
Jonathan Protzenko <protz@microsoft.com>
2023-09-13 17:17:47 -0700

    Extra file

---
+++ tests/src/array2.rs
@@ -0,0 +1,11 @@
+//! Exercise the translation of arrays, features not yet supported by Eurydice
+#![allow(dead_code)]
+
+fn index_array_generic<const N: usize>(s: [u32; N], i: usize) -> u32 {
+    s[i]
+}
+
+fn index_array_generic_call<const N: usize>(s: [u32; N], i: usize) -> u32 {
+    index_array_generic(s, i)
+}
+
