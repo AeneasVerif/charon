5075a9df1e4f06be1b28ec1e9f5330c7b4db48d7
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2023-09-22 16:37:29 +0200

    Both Shl and Shr can panic during under/overflows

--- charon-ml/src/ExpressionsUtils.ml
+++ charon-ml/src/ExpressionsUtils.ml
@@ -6,5 +6,4 @@ let unop_can_fail (unop : E.unop) : bool =
 let binop_can_fail (binop : E.binop) : bool =
   match binop with
   | BitXor | BitAnd | BitOr | Eq | Lt | Le | Ne | Ge | Gt -> false
-  | Div | Rem | Add | Sub | Mul -> true
-  | Shl | Shr -> raise Utils.Unimplemented
+  | Div | Rem | Add | Sub | Mul | Shl | Shr -> true
