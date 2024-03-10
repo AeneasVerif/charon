5c0405c2f303c303b228c32e79976ce62fc29b73
Son Ho <hosonmarc@gmail.com>
2024-03-10 19:27:34 +0100

    Add visitors for the name patterns

--- charon-ml/name_matcher_parser/Ast.ml
+++ charon-ml/name_matcher_parser/Ast.ml
@@ -7,18 +7,27 @@ let pp_big_int (fmt : Format.formatter) (bi : big_int) : unit =
 let compare_big_int (bi0 : big_int) (bi1 : big_int) : int = Z.compare bi0 bi1
 let show_big_int (bi : big_int) : string = Z.to_string bi
 
-type var = VarName of string | VarIndex of int [@@deriving show, ord]
+(** Ancestor the pattern iter visitor *)
+class ['self] iter_pattern_base =
+  object (_self : 'self)
+    inherit [_] VisitorsRuntime.iter
+    method visit_big_int : 'env -> big_int -> unit = fun _ _ -> ()
+  end
 
-type literal = LInt of big_int | LBool of bool | LChar of char
-[@@deriving show, ord]
+(** Ancestor the pattern map visitor *)
+class ['self] map_pattern_base =
+  object (_self : 'self)
+    inherit [_] VisitorsRuntime.map
+    method visit_big_int : 'env -> big_int -> big_int = fun _ x -> x
+  end
 
-(*type const_generic = CgVar of const_generic_var option | CgValue of Z.t*)
-type ref_kind = RMut | RShared [@@deriving show, ord]
-type region = RVar of var option | RStatic [@@deriving show, ord]
-type primitive_adt = TTuple | TArray | TSlice [@@deriving show, ord]
-type mutability = Mut | Not [@@deriving show, ord]
-
-type pattern = pattern_elem list
+type var = VarName of string | VarIndex of int
+and literal = LInt of big_int | LBool of bool | LChar of char
+and ref_kind = RMut | RShared
+and region = RVar of var option | RStatic
+and primitive_adt = TTuple | TArray | TSlice
+and mutability = Mut | Not
+and pattern = pattern_elem list
 and pattern_elem = PIdent of string * generic_args | PImpl of expr
 
 (** An expression can be a type or a trait ref.
@@ -39,4 +48,26 @@ and expr =
   | ERawPtr of mutability * expr
 
 and generic_arg = GExpr of expr | GValue of literal | GRegion of region
-and generic_args = generic_arg list [@@deriving show, ord]
+
+and generic_args = generic_arg list
+[@@deriving
+  show,
+    ord,
+    visitors
+      {
+        name = "iter_pattern";
+        variety = "iter";
+        ancestors = [ "iter_pattern_base" ];
+        nude = true (* Don't inherit {!VisitorsRuntime.iter} *);
+        concrete = true;
+        polymorphic = false;
+      },
+    visitors
+      {
+        name = "map_pattern";
+        variety = "map";
+        ancestors = [ "map_pattern_base" ];
+        nude = true (* Don't inherit {!VisitorsRuntime.map} *);
+        concrete = false;
+        polymorphic = false;
+      }]
