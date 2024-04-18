a2e3f675df3965d937d69cb819b0a1e0a3da4a55
Nadrieril <nadrieril+git@gmail.com>
2024-04-18 09:48:32 +0200

    Add tests

---
+++ charon/tests/ui/issue-120-bare-discriminant-read.rs
@@ -0,0 +1,16 @@
+//@ known-failure
+//@ charon-args=--extract-opaque-bodies
+//@ charon-args=--mir_optimized
+//@ rustc-args=-C opt-level=3
+
+fn call_is_some<T>(opt: Option<T>) -> bool {
+    opt.is_some()
+}
+
+// This doesn't optimize to a bare discriminant read :(
+fn my_is_some<T>(opt: Option<T>) -> isize {
+    match opt {
+        Some(_) => 1,
+        None => 0,
+    }
+}
--- charon/tests/ui/unsupported/option-is_some.rs
+++
@@ -1,7 +0,0 @@
-//@ known-failure
-//@ charon-args=--extract-opaque-bodies
-
-// Panics because the optimized MIR has a discriminant read not followed by a SwitchInt.
-fn my_is_some<T>(opt: Option<T>) -> bool {
-    opt.is_some()
-}
