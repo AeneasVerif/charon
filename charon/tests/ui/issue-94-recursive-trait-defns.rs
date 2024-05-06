4b8ff0e0b7b414af55e6bc500a23535074a5bfa8
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2024-05-06 12:41:05 +0200

    Add more recursive traits examples

--- charon/tests/ui/issue-94-recursive-trait-defns.rs
+++ charon/tests/ui/issue-94-recursive-trait-defns.rs
@@ -4,5 +4,24 @@ pub trait Trait1 {
 
 pub trait Trait2: Trait1 {}
 
-trait T1<T: T2<Self>>: Sized {}
-trait T2<T: T1<Self>>: Sized {}
+pub trait T1<T: T2<Self>>: Sized {}
+pub trait T2<T: T1<Self>>: Sized {}
+
+
+pub trait T3 {
+    type T: T5;
+}
+
+pub trait T4: T3 {}
+
+pub trait T5 {
+    type T: T4;
+}
+
+pub trait T6<T: T7> {
+  fn f(x:u64) -> () { T::g(x) }
+}  
+
+pub trait T7: T6<Self> {
+  fn g(_x:u64) -> ();
+}
