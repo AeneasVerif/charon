07d365a292604ed02408f85c709e385eb2fc6432
Nadrieril <nadrieril+git@gmail.com>
2024-05-24 10:44:00 +0200

    Correctly parse `TraitInstanceId` in `charon-ml`

---
+++ charon/tests/ui/trait-instance-id.rs
@@ -0,0 +1,21 @@
+//@ charon-args=--errors-as-warnings
+// This (for now) produces `TraitInstanceId::Unsolved`; it's a regressiontest because we used to
+// not parse this in `charon-ml`.
+fn main() {
+    let a = [0, 1, 2, 3, 4, 5, 6];
+    let mut i = 0;
+    for v in a.into_iter() {
+        i += v;
+    }
+    for v in a.iter() {
+        i += v;
+    }
+    for _ in a.chunks(2) {
+        i += 1;
+    }
+    for _ in a.chunks_exact(2) {
+        i += 1;
+    }
+    let expected = 28;
+    assert_eq!(i, expected);
+}
