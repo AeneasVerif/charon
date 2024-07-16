9f7c5431b4bc609d9685aff8f1f9e72a52c33a4e
Nadrieril <Nadrieril@users.noreply.github.com>
2024-07-16 15:59:53 +0200

    Tweak comment

--- charon/src/ast/llbc_ast.rs
+++ charon/src/ast/llbc_ast.rs
@@ -27,10 +27,8 @@ pub struct Assert {
     Debug, Clone, EnumIsA, EnumToGetters, EnumAsGetters, Serialize, Deserialize, Drive, DriveMut,
 )]
 pub enum RawStatement {
-    /// Assigns an `Rvalue` to a `Place`. e.g. `let y = x` becomes
-    /// `y := copy x` which looks like
-    /// `"Assign":(("var_id":2,"projection":[]),("Use":("Copy":("var_id":1,
-    /// "projection":[]))))` in llbc
+    /// Assigns an `Rvalue` to a `Place`. e.g. `let y = x;` could become
+    /// `y := move x` which is represented as `Assign(y, Rvalue::Use(Operand::Move(x)))`.
     Assign(Place, Rvalue),
     /// Only used for borrow-checking
     FakeRead(Place),
