ca594cd4b300af4b62e50eb718724e26b109e5db
Nadrieril <nadrieril+git@gmail.com>
2024-08-09 11:45:39 +0200

    `RegionKind::ReVar` doesn't exist outside of type inference

--- charon/src/bin/charon-driver/translate/translate_types.rs
+++ charon/src/bin/charon-driver/translate/translate_types.rs
@@ -81,63 +81,9 @@ impl<'tcx, 'ctx, 'ctx1> BodyTransCtx<'tcx, 'ctx, 'ctx1> {
                     let br_id = DeBruijnId::new(*id);
                     Ok(Region::BVar(br_id, *rid))
                 }
-                hax::RegionKind::ReVar(re_var) => {
-                    // TODO: I'm really not sure how to handle those, here.
-                    // They sometimes appear and seem to refer to the early bound
-                    // regions. But on the other hand, whenever I investigated, I
-                    // only encountered those in *trait references* in trait
-                    // implementations.
-                    //
-                    // For instance, here is a minimal example which triggers
-                    // this case:
-                    // ```
-                    // pub trait From<T> {
-                    //     type Error;
-                    //   fn from(v: T) -> Result<Self, Self::Error>
-                    // //                                ^^^^^^^^^^
-                    // //                    This associated type is important
-                    //     where
-                    //         Self: std::marker::Sized;
-                    //
-                    // impl From<&bool> for bool {
-                    // //        ^^^^^
-                    // // This reference is important
-                    //     type Error = ();
-                    //
-                    //     fn from(v: &bool) -> Result<Self, Self::Error> {
-                    //         Ok(*v)
-                    //     }
-                    // }
-                    // ```
-                    //
-                    // If we extract this to LLBC, wet get (focusing on the implementation
-                    //  of `from`):
-                    // ```
-                    // ... // omitted
-                    //
-                    // fn crate::{bool}::from<@R0, @R1>(@1: &@R1 (bool)) ->
-                    //   core::result::Result<bool, crate::{bool}<@R0>::Error> {
-                    //   //                                       ^^^
-                    //   //                             The problematic region
-                    //   ... // omitted
-                    // }
-                    // ```
-                    error_assert!(self, span, self.free_region_vars.get(region).is_none());
-
-                    for (rk, rid) in self.free_region_vars.iter() {
-                        if let hax::RegionKind::ReEarlyParam(eb) = &rk.kind {
-                            if eb.index as usize == *re_var {
-                                // Note that the DeBruijn index depends
-                                // on the current stack of bound region groups.
-                                let db_id = self.region_vars.len() - 1;
-                                return Ok(Region::BVar(DeBruijnId::new(db_id), *rid));
-                            }
-                        }
-                    }
-                    let err = format!(
-                        "Could not find region: {:?}\n\nRegion vars map:\n{:?}\n\nBound region vars:\n{:?}",
-                        region, self.free_region_vars, self.bound_region_vars
-                    );
+                hax::RegionKind::ReVar(_) => {
+                    // Shouldn't exist outside of type inference.
+                    let err = format!("Should not exist outside of type inference: {region:?}");
                     error_or_panic!(self, span, err)
                 }
                 _ => {
