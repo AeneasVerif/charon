14f2b487cd98f4bb6996cca4d8cfd7b7bb5cf85e
Nadrieril <nadrieril+git@gmail.com>
2024-08-20 11:52:54 +0200

    Always name trait methods

--- charon/src/bin/charon-driver/translate/translate_traits.rs
+++ charon/src/bin/charon-driver/translate/translate_traits.rs
@@ -166,19 +166,10 @@ impl<'tcx, 'ctx> TranslateCtx<'tcx, 'ctx> {
             let item_span = bt_ctx.t_ctx.tcx.def_span(rust_item_id);
             match &hax_item.kind {
                 AssocKind::Fn => {
-                    // Skip the provided methods for the *external* trait declarations,
-                    // but still remember their name (unless `extract_opaque_bodies` is set).
-                    // TODO: translate function signatures unconditionally.
                     if hax_item.has_value {
                         // This is a *provided* method,
-                        // TODO: this branch is a hack: we should always give an id to methods and
-                        // skip translating their body. However today we run into `for<'a>` crashes.
-                        if rust_id.is_local() || bt_ctx.t_ctx.options.extract_opaque_bodies {
-                            let fun_id = bt_ctx.register_fun_decl_id(item_span, rust_item_id);
-                            provided_methods.push((item_name.clone(), Some(fun_id)));
-                        } else {
-                            provided_methods.push((item_name.clone(), None));
-                        }
+                        let fun_id = bt_ctx.register_fun_decl_id(item_span, rust_item_id);
+                        provided_methods.push((item_name.clone(), Some(fun_id)));
                     } else {
                         // This is a required method (no default implementation)
                         let fun_id = bt_ctx.register_fun_decl_id(item_span, rust_item_id);
