b6bb0814c060d4af1e0dd283cbedd2a23d287d8f
Nadrieril <nadrieril+git@gmail.com>
2024-09-30 18:48:09 +0200

    Simplify `update_block_indices`

--- charon/src/transform/update_block_indices.rs
+++ charon/src/transform/update_block_indices.rs
@@ -1,5 +1,9 @@
 //! Update the block indices to make sure they are consecutive
 
+use std::mem;
+
+use derive_visitor::{visitor_enter_fn_mut, DriveMut};
+
 use crate::ids::*;
 use crate::transform::TransformCtx;
 use crate::ullbc_ast::*;
@@ -9,39 +13,15 @@ use super::ctx::UllbcPass;
 pub struct Transform;
 impl UllbcPass for Transform {
     fn transform_body(&self, _ctx: &mut TransformCtx<'_>, b: &mut ExprBody) {
-        // Compute the map from old ids to new ids
-        let mut id_map: MapGenerator<BlockId, BlockId> = MapGenerator::new();
-        for (id, _) in b.body.iter_indexed() {
-            let _ = id_map.insert(id);
-        }
+        // Push each block into a new vector to make it consecutive and return the map from old to
+        // new ids.
+        let id_map: Vector<BlockId, BlockId> =
+            mem::take(&mut b.body).map(|block| b.body.push(block));
 
-        // Update the ids
-        take_mut::take(&mut b.body, |mut body| {
-            // Update the ids
-            body.iter_mut().for_each(|block| {
-                use RawTerminator::*;
-                match &mut block.terminator.content {
-                    Goto { target } => *target = id_map.get(target).unwrap(),
-                    Switch { targets, .. } => {
-                        use SwitchTargets::*;
-                        match targets {
-                            If(id0, id1) => {
-                                *id0 = id_map.get(id0).unwrap();
-                                *id1 = id_map.get(id1).unwrap();
-                            }
-                            SwitchInt(_, targets, otherwise) => {
-                                for (_, id) in targets {
-                                    *id = id_map.get(id).unwrap();
-                                }
-                                *otherwise = id_map.get(otherwise).unwrap();
-                            }
-                        }
-                    }
-                    Abort(_) | Return => (),
-                }
-            });
-            // Reconstruct the vector to make sure there are no holes
-            body.into_iter().collect()
-        });
+        // Update the ids.
+        b.body
+            .drive_mut(&mut visitor_enter_fn_mut(|id: &mut BlockId| {
+                *id = id_map[*id]
+            }));
     }
 }
