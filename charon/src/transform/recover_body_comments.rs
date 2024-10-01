261e96f2da25e255b535dcdd9b0019af45ba7636
Nadrieril <nadrieril+git@gmail.com>
2024-10-01 17:34:59 +0200

    tmp

--- charon/src/transform/recover_body_comments.rs
+++ charon/src/transform/recover_body_comments.rs
@@ -1,6 +1,7 @@
 //! Take all the comments found in the original body and assign them to statements.
 
-use std::collections::HashMap;
+use std::cmp::max;
+use std::collections::{HashMap, HashSet};
 
 use derive_visitor::{visitor_enter_fn, visitor_enter_fn_mut, Drive, DriveMut};
 
@@ -12,11 +13,30 @@ use super::ctx::LlbcPass;
 pub struct Transform;
 impl LlbcPass for Transform {
     // Constraints in the ideal case:
-    // - each comment should be assigned to exactly one statement;
+    // - each comment should be assigned to at most one statement;
     // - the order of comments in the source should refine the partial order of control flow;
     // - a comment should come before the statement it was applied to.
     // We approximate this with a reasonable heuristic.
+    //
+    // We may drop some comments if no statement starts with the relevant line (this can happen if
+    // e.g. the statement was optimized out or the comment applied to an item instead).
     fn transform_body(&self, _ctx: &mut TransformCtx<'_>, b: &mut ExprBody) {
+        // For each source line (that a comment may apply to), we try to compute the set of lines
+        // that are spanned by the statement/expression that starts on that line. This assumes
+        // standard one-statement-per-line rust formatting.
+        // We store for each start line the end line.
+        let mut lines_covered_by_statement: HashMap<usize, usize> = Default::default();
+        b.body.drive(&mut visitor_enter_fn(|st: &Statement| {
+            let span = st.span;
+            let end_line = lines_covered_by_statement
+                .entry(span.span.beg.line)
+                .or_insert(span.span.beg.line);
+            *end_line = max(*end_line, span.span.end.line);
+        }));
+
+        // TODO: for each syntactic line, find the span of the corresponding semantic line if
+        // possible.
+        // TODO: order by statement kind: call, assign
         // Find for each line the statement span that starts the earliest as this is more likely to
         // correspond to what the comment was intended to point to.
         let mut best_span_for_line: HashMap<usize, Span> = Default::default();
@@ -48,9 +68,6 @@ impl LlbcPass for Transform {
             .map(|(loc, comments)| (loc.line, comments))
             .collect();
         // Assign each comment to the first statement that has the best span for its starting line.
-        // This may drop some comments if no statement starts with the relevant line (this can
-        // happen if e.g. the statement was optimized out or the comment applied to an item
-        // instead).
         b.body
             .drive_mut(&mut visitor_enter_fn_mut(|st: &mut Statement| {
                 if best_span_for_line
