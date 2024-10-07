e34f20c7dbb2e5841ab3b8188b43a77435f1aa35
Nadrieril <nadrieril+git@gmail.com>
2024-10-07 17:09:25 +0200

    Revert "Remove the top-level `catch_unwind`"
    This reverts commit 7a9e32e7335f616aee752429d4348fc5ad1857c5.

--- charon/src/bin/charon-driver/main.rs
+++ charon/src/bin/charon-driver/main.rs
@@ -237,11 +237,16 @@ fn main() {
                 log::warn!("{}", msg);
             }
         }
-        Err(err) => {
+        Err(err) if errors_as_warnings => {
             log::error!("{err}");
-            if !errors_as_warnings {
-                std::process::exit(1);
-            }
+        }
+        Err(CharonFailure::Panic) => {
+            // Standard rust panic error code.
+            std::process::exit(101);
+        }
+        Err(err @ (CharonFailure::RustcError(_) | CharonFailure::Serialize)) => {
+            log::error!("{err}");
+            std::process::exit(1);
         }
     }
 }
