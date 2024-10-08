74ff6e4faf215ccb1f483106270c30931d4de4f7
Nadrieril <nadrieril+git@gmail.com>
2024-10-08 14:56:50 +0200

    Add test

--- charon/tests/ui/stealing.rs
+++ charon/tests/ui/stealing.rs
@@ -1,3 +1,5 @@
+//@ known-failure
+
 // Translating this needs the evaluatable MIR of `fn four()`, which steals its `mir_built` body.
 // There's no simple ordering of the translation of items that can avoid all stealing.
 static SLICE: [(); four()] = [(); 4];
@@ -5,3 +7,7 @@ static SLICE: [(); four()] = [(); 4];
 const fn four() -> usize {
     2 + 2
 }
+
+// The order counts, we want to translate `BAR` first to steal `FOO`.
+const BAR: [(); FOO] = [(); FOO];
+const FOO: usize = 42;
