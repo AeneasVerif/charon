aadd9a2ca22ee9bf0a6f45019ad1b98080b837e3
Nadrieril <nadrieril+git@gmail.com>
2024-10-08 15:00:22 +0200

    Don't use optimized_mir for constants

--- charon/tests/ui/stealing.rs
+++ charon/tests/ui/stealing.rs
@@ -1,5 +1,3 @@
-//@ known-failure
-
 // Translating this needs the evaluatable MIR of `fn four()`, which steals its `mir_built` body.
 // There's no simple ordering of the translation of items that can avoid all stealing.
 static SLICE: [(); four()] = [(); 4];
