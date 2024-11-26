b39463557372973a74ba659b48185f92d8013153
Nadrieril <nadrieril+git@gmail.com>
2024-11-26 16:43:40 +0100

    Move rust name matcher tests to a standalone file

--- charon/tests/test-rust-name-matcher.rs
+++ charon/tests/test-rust-name-matcher.rs
@@ -7,6 +7,8 @@ use charon_lib::name_matcher::Pattern;
 
 mod util;
 
+static TEST_FILE: &str = "tests/ui/rust-name-matcher-tests.rs";
+
 fn parse_pattern_attr(a: &Attribute) -> Option<(bool, Pattern)> {
     let Attribute::Unknown(a) = a else {
         return None;
@@ -30,65 +32,7 @@ fn parse_pattern_attr(a: &Attribute) -> Option<(bool, Pattern)> {
 
 #[test]
 fn test_name_matcher() -> anyhow::Result<()> {
-    let crate_data = util::translate_rust_text(
-        r#"
-        #![feature(register_tool)]
-        #![register_tool(pattern)]
-        mod foo {
-            #[pattern::fail("test_crate::foo::bar::_")]
-            #[pattern::pass("test_crate::foo::bar")]
-            #[pattern::pass("test_crate::foo::_")]
-            #[pattern::pass("test_crate::foo")]
-            #[pattern::pass("test_crate")]
-            #[pattern::pass("test_crate::_")]
-            #[pattern::pass("test_crate::_::bar")]
-            #[pattern::fail("test_crate::_::lsdkfjs")]
-            #[pattern::pass("crate::foo::bar")]
-            #[pattern::fail("foo::bar")]
-            fn bar() {}
-        }
-
-        #[pattern::pass("test_crate::Trait")]
-        #[pattern::fail("test_crate::Trait<_>")]
-        #[pattern::fail("test_crate::Trait<_, _>")]
-        trait Trait<T> {
-            #[pattern::pass("test_crate::Trait::method")]
-            #[pattern::fail("test_crate::Trait<_>::method")]
-            fn method<U>();
-        }
-
-        #[pattern::pass("test_crate::{impl test_crate::Trait<_> for _}")]
-        #[pattern::pass("test_crate::{impl test_crate::Trait<_, _>}")]
-        #[pattern::fail("test_crate::{impl test_crate::Trait<_, _> for _}")]
-        #[pattern::pass("test_crate::{impl test_crate::Trait<core::option::Option<_>> for alloc::boxed::Box<_>}")]
-        #[pattern::pass("test_crate::{impl test_crate::Trait<alloc::boxed::Box<_>, core::option::Option<_>>}")]
-        #[pattern::pass("test_crate::{impl test_crate::Trait<core::option::Option<_>> for alloc::boxed::Box<_>}")]
-        #[pattern::fail("test_crate::{impl test_crate::Trait<Option<_>> for alloc::boxed::Box<_>}")]
-        #[pattern::fail("test_crate::{impl test_crate::Trait<FooBar<_>> for alloc::boxed::Box<_>}")]
-        #[pattern::fail("test_crate::{impl test_crate::Trait<core::option::Option<_>> for FooBar<_>}")]
-        #[pattern::fail("test_crate::{impl Trait<_> for _}")]
-        #[pattern::fail("test_crate::{impl test_crate::OtherTrait<_> for _}")]
-        #[pattern::fail("test_crate::Trait<_>")]
-        impl<T> Trait<Option<T>> for Box<T> {
-            #[pattern::pass("test_crate::{impl test_crate::Trait<_> for _}::method")]
-            #[pattern::pass("test_crate::{impl test_crate::Trait<_> for _}::_")]
-            #[pattern::pass("test_crate::{impl test_crate::Trait<_> for _}")]
-            #[pattern::pass("test_crate::{impl test_crate::Trait<_, _>}"::method)]
-            #[pattern::fail("test_crate::Trait<_>::method")]
-            fn method<U>() {}
-        }
-
-        #[pattern::pass("test_crate::{impl test_crate::Trait<_> for Slice<_>}")]
-        impl<T> Trait<T> for [T] {
-            fn method<U>() {}
-        }
-
-        #[pattern::pass("test_crate::{impl test_crate::Trait<_> for &Slice<_>}")]
-        impl<T> Trait<T> for &[T] {
-            fn method<U>() {}
-        }
-        "#,
-    )?;
+    let crate_data = util::translate_rust_text(&std::fs::read_to_string(TEST_FILE)?)?;
     let fmt_ctx = &crate_data.into_fmt();
 
     for item in crate_data.all_items() {
