48b96573397077331bc84781bdde7985d16e56c7
Nadrieril <nadrieril+git@gmail.com>
2025-01-17 13:21:14 +0100

    Add tests

---
+++ charon/tests/ui/simple/call-inherent-method-with-trait-bound.rs
@@ -0,0 +1,20 @@
+pub trait Trait {
+    type Type;
+}
+
+impl Trait for () {
+    type Type = ();
+}
+
+pub struct HashMap<S>(S);
+
+impl<S> HashMap<S> {
+    pub fn get<Q: Trait>(_x: HashMap<S>, _k: Q) {}
+}
+
+pub fn top_level_get<S, Q: Trait>(_x: HashMap<S>, _k: Q) {}
+
+pub fn test(map: HashMap<()>) {
+    top_level_get(map, ());
+    HashMap::get(map, ());
+}
---
+++ charon/tests/ui/simple/pass-higher-kinded-fn-item-as-closure.rs
@@ -0,0 +1,9 @@
+pub fn flabada<'a>(x: &'a ()) -> &'a () {
+    x
+}
+
+pub fn call<'b, F: Fn(&'b ()) -> &'b ()>(_: F) {}
+
+pub fn flibidi() {
+    call(flabada);
+}
