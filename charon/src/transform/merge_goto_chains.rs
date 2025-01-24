d3d272d5f4866c5d20244e680ef6bd814d80b0a4
Nadrieril <nadrieril+git@gmail.com>
2025-01-24 13:26:05 +0100

    Fix `merge_goto_chains`

--- charon/src/transform/merge_goto_chains.rs
+++ charon/src/transform/merge_goto_chains.rs
@@ -9,15 +9,19 @@ use super::ctx::UllbcPass;
 /// antecedent.
 enum Antecedents {
     Zero,
-    One(BlockId),
+    One {
+        id: BlockId,
+        /// Whether the antecedent is a goto.
+        is_goto: bool,
+    },
     Many,
 }
 
 impl Antecedents {
-    fn add(&mut self, id: BlockId) {
+    fn add(&mut self, id: BlockId, is_goto: bool) {
         match self {
-            Antecedents::Zero => *self = Antecedents::One(id),
-            Antecedents::One(_) => *self = Antecedents::Many,
+            Antecedents::Zero => *self = Antecedents::One { id, is_goto },
+            Antecedents::One { .. } => *self = Antecedents::Many,
             Antecedents::Many => {}
         }
     }
@@ -35,8 +39,9 @@ impl UllbcPass for Transform {
         let mut antecedents: Vector<BlockId, Antecedents> =
             body.body.map_ref(|_| Antecedents::Zero);
         for (block_id, block) in body.body.iter_indexed() {
+            let is_goto = block.terminator.content.is_goto();
             for target in block.targets() {
-                antecedents.get_mut(target).unwrap().add(block_id);
+                antecedents.get_mut(target).unwrap().add(block_id, is_goto);
             }
         }
         // Merge blocks with a single antecedent into their antecedent.
@@ -44,14 +49,18 @@ impl UllbcPass for Transform {
             // Go up the chain to find the first parent with zero (the start block) or multiple
             // antecedents. This avoids quadratic behavior where we repeatedly copy a growing list
             // of statements, since blocks may not be sorted..
-            while let Antecedents::One(antecedent) = antecedents[id] {
-                id = antecedent;
+            while let Antecedents::One {
+                id: antecedent_id,
+                is_goto: true,
+            } = antecedents[id]
+            {
+                id = antecedent_id;
             }
             // While the current block is a straight goto, merge the target block back into this
             // one.
             while let Some(source) = body.body.get(id)
                 && let RawTerminator::Goto { target } = source.terminator.content
-                && let Antecedents::One(_) = antecedents[target]
+                && let Antecedents::One { .. } = antecedents[target]
             {
                 let mut target = body.body.remove(target).unwrap();
                 let source = &mut body.body[id];
