28ffe082cababc0696fd919cc944169b6b9c56d2
Nadrieril <nadrieril+git@gmail.com>
2025-03-07 14:17:49 +0100

    Add tests

---
+++ charon/tests/ui/simple/mem-discriminant-from-derive.rs
@@ -0,0 +1,9 @@
+//@ charon-args=--remove-associated-types=*
+//@ known-failure
+// This derive generates code that uses `mem::discriminant`, which involves the builtin associated
+// type `<T as DiscriminantKind>::Discriminant`.
+#[derive(PartialEq)]
+enum Enum {
+    Some(u8),
+    None,
+}
---
+++ charon/tests/ui/simple/pointee_metadata.rs
@@ -0,0 +1,11 @@
+//@ charon-args=--remove-associated-types=*
+//@ known-failure
+#![feature(ptr_metadata)]
+
+fn empty_metadata() {
+    let _ = (&0u32 as *const u32).to_raw_parts();
+}
+
+fn slice_metadata() {
+    let _ = (&[0u32, 1u32] as *const [u32]).to_raw_parts();
+}
