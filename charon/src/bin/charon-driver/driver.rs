99c0d4a964074c3cd2f34c430a6195bcc73795de
Nadrieril <nadrieril+git@gmail.com>
2025-03-21 16:02:07 +0100

    Centralize option setting even more

--- charon/src/bin/charon-driver/driver.rs
+++ charon/src/bin/charon-driver/driver.rs
@@ -6,12 +6,24 @@ use charon_lib::transform::TransformCtx;
 use rustc_driver::{Callbacks, Compilation};
 use rustc_interface::Config;
 use rustc_interface::{interface::Compiler, Queries};
+use rustc_middle::util::Providers;
 use rustc_session::config::{OutputType, OutputTypes, Polonius};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::{env, fmt};
 
-/// Disable all these mir passes.
-fn disabled_mir_passes(config: &mut Config) {
+/// Helper that runs the compiler and catches its fatal errors.
+fn run_compiler_with_callbacks(
+    args: Vec<String>,
+    callbacks: &mut (dyn Callbacks + Send),
+) -> Result<(), CharonFailure> {
+    rustc_driver::catch_fatal_errors(|| rustc_driver::RunCompiler::new(&args, callbacks).run())
+        .map_err(|_| CharonFailure::RustcError)?
+        .map_err(|_| CharonFailure::RustcError)?;
+    Ok(())
+}
+
+/// Disable all the mir passes we can disable without causing ICEs.
+fn disable_mir_opt_passes(config: &mut Config) {
     let disabled_mir_passes = [
         "AfterConstProp",
         "AfterGVN",
@@ -60,7 +72,7 @@ fn disabled_mir_passes(config: &mut Config) {
 
 /// Don't even try to codegen. This avoids errors due to checking if the output filename is
 /// available (despite the fact that we won't emit it because we stop compilation early).
-fn no_codegen(config: &mut Config) {
+fn set_no_codegen(config: &mut Config) {
     config.opts.unstable_opts.no_codegen = true;
 
     // i.e. --emit=metadata
@@ -75,16 +87,59 @@ fn no_codegen(config: &mut Config) {
 /// code. Also, rustc inserts a lot of dynamic checks in debug mode, that we
 /// have to clean. Full list of `--release` flags:
 /// https://doc.rust-lang.org/cargo/reference/profiles.html#release
-fn release_mode(config: &mut Config) {
+fn set_release_mode(config: &mut Config) {
     let cg = &mut config.opts.cg;
     cg.opt_level = "3".into();
     cg.overflow_checks = Some(false);
     config.opts.debug_assertions = false;
 }
 
-/// -Zpolonius
-fn polonius(config: &mut Config, enable: bool) {
-    if enable {
+// We use a static to be able to pass data to `override_queries`.
+static SKIP_BORROWCK: AtomicBool = AtomicBool::new(false);
+fn set_skip_borrowck() {
+    SKIP_BORROWCK.store(true, Ordering::SeqCst);
+}
+fn skip_borrowck_if_set(providers: &mut Providers) {
+    if SKIP_BORROWCK.load(Ordering::SeqCst) {
+        providers.mir_borrowck = |tcx, def_id| {
+            let (input_body, _promoted) = tcx.mir_promoted(def_id);
+            let input_body = &input_body.borrow();
+            // Empty result, which is what is used for tainted or custom_mir bodies.
+            let result = rustc_middle::mir::BorrowCheckResult {
+                concrete_opaque_types: Default::default(),
+                closure_requirements: None,
+                used_mut_upvars: Default::default(),
+                tainted_by_errors: input_body.tainted_by_errors,
+            };
+            tcx.arena.alloc(result)
+        }
+    }
+}
+
+fn setup_compiler(config: &mut Config, options: &CliOpts, do_translate: bool) {
+    if do_translate {
+        if options.skip_borrowck {
+            // We use a static to be able to pass data to `override_queries`.
+            set_skip_borrowck();
+        }
+
+        config.override_queries = Some(|_sess, providers| {
+            skip_borrowck_if_set(providers);
+
+            // TODO: catch the MIR in-flight to avoid stealing issues?
+            // providers.mir_built = |tcx, def_id| {
+            //     let mir = (rustc_interface::DEFAULT_QUERY_PROVIDERS.mir_built)(tcx, def_id);
+            //     let mut mir = mir.steal();
+            //     // use the mir
+            //     tcx.alloc_steal_mir(mir)
+            // };
+        });
+
+        disable_mir_opt_passes(config);
+        set_release_mode(config);
+        set_no_codegen(config);
+    }
+    if options.use_polonius {
         config.opts.unstable_opts.polonius = Polonius::Legacy;
     }
 }
@@ -95,7 +150,7 @@ fn polonius(config: &mut Config, enable: bool) {
 pub fn run_rustc_driver(options: &CliOpts) -> Result<Option<TransformCtx>, CharonFailure> {
     // Retreive the command-line arguments pased to `charon_driver`. The first arg is the path to
     // the current executable, we skip it.
-    let compiler_args: Vec<String> = env::args().skip(1).collect();
+    let mut compiler_args: Vec<String> = env::args().skip(1).collect();
 
     // Cargo calls the driver twice. The first call to the driver is with "--crate-name ___" and no
     // source file, for Cargo to retrieve some information about the crate.
@@ -119,55 +174,27 @@ pub fn run_rustc_driver(options: &CliOpts) -> Result<Option<TransformCtx>, Charo
     let is_target = arg_values(&compiler_args, "--target").next().is_some();
 
     let no_translate = is_dry_run || is_workspace_dependency || !is_target;
-    if no_translate {
+    let output = if no_translate {
         trace!("Skipping charon; running compiler normally instead.");
-        // In this case we run the compiler normally.
-        RunCompilerNormallyCallbacks::new(options)
-            .run_compiler(compiler_args)
-            .unwrap();
-        return Ok(None);
-    }
+        // Run the compiler normally.
+        run_compiler_with_callbacks(compiler_args, &mut RunCompilerNormallyCallbacks { options })?;
+        None
+    } else {
+        for extra_flag in options.rustc_args.iter().cloned() {
+            compiler_args.push(extra_flag);
+        }
 
-    trace!("Compiler arguments: {:?}", compiler_args);
-
-    // Call the Rust compiler with our custom callback.
-    let mut callback = CharonCallbacks::new(options);
-    callback.run_compiler(compiler_args)?;
-    // If `transform_ctx` is not set here, there was a fatal error.
-    let ctx = callback.transform_ctx.ok_or(CharonFailure::RustcError)?;
-    Ok(Some(ctx))
-}
-
-/// The callbacks for Charon
-pub struct CharonCallbacks<'a> {
-    pub options: &'a CliOpts,
-    /// This is to be filled during the extraction; it contains the translated crate.
-    transform_ctx: Option<TransformCtx>,
-}
-
-impl<'a> CharonCallbacks<'a> {
-    pub fn new(options: &'a CliOpts) -> Self {
-        Self {
+        // Call the Rust compiler with our custom callback.
+        let mut callback = CharonCallbacks {
             options,
             transform_ctx: None,
-        }
-    }
-
-    /// Run rustc with our custom callbacks. `args` is the arguments passed to `rustc`'s
-    /// command-line.
-    pub fn run_compiler(&mut self, mut args: Vec<String>) -> Result<(), CharonFailure> {
-        for extra_flag in self.options.rustc_args.iter().cloned() {
-            args.push(extra_flag);
-        }
-
-        rustc_driver::catch_fatal_errors(|| {
-            rustc_driver::RunCompiler::new(&args, self)
-                .run()
-                .map_err(|_| CharonFailure::RustcError)
-        })
-        .map_err(|_| CharonFailure::RustcError)??;
-        Ok(())
-    }
+        };
+        run_compiler_with_callbacks(compiler_args, &mut callback)?;
+        // If `transform_ctx` is not set here, there was a fatal error.
+        let ctx = callback.transform_ctx.ok_or(CharonFailure::RustcError)?;
+        Some(ctx)
+    };
+    Ok(output)
 }
 
 /// Custom `DefId` debug routine that doesn't print unstable values like ids and hashes.
@@ -192,44 +219,15 @@ fn def_id_debug(def_id: rustc_hir::def_id::DefId, f: &mut fmt::Formatter<'_>) ->
     })
 }
 
+/// The callbacks for Charon
+pub struct CharonCallbacks<'a> {
+    pub options: &'a CliOpts,
+    /// This is to be filled during the extraction; it contains the translated crate.
+    transform_ctx: Option<TransformCtx>,
+}
 impl<'a> Callbacks for CharonCallbacks<'a> {
     fn config(&mut self, config: &mut Config) {
-        // We use a static to be able to pass data to `override_queries`.
-        static SKIP_BORROWCK: AtomicBool = AtomicBool::new(false);
-        if self.options.skip_borrowck {
-            SKIP_BORROWCK.store(true, Ordering::SeqCst);
-        }
-
-        config.override_queries = Some(|_sess, providers| {
-            // TODO: catch the MIR in-flight to avoid stealing issues.
-            // providers.mir_built = |tcx, def_id| {
-            //     let mir = (rustc_interface::DEFAULT_QUERY_PROVIDERS.mir_built)(tcx, def_id);
-            //     let mut mir = mir.steal();
-            //     // use the mir
-            //     tcx.alloc_steal_mir(mir)
-            // };
-
-            if SKIP_BORROWCK.load(Ordering::SeqCst) {
-                providers.mir_borrowck = |tcx, def_id| {
-                    let (input_body, _promoted) = tcx.mir_promoted(def_id);
-                    let input_body = &input_body.borrow();
-                    // Empty result, which is what is used for tainted or custom_mir bodies.
-                    let result = rustc_middle::mir::BorrowCheckResult {
-                        concrete_opaque_types: Default::default(),
-                        closure_requirements: None,
-                        used_mut_upvars: Default::default(),
-                        tainted_by_errors: input_body.tainted_by_errors,
-                    };
-                    tcx.arena.alloc(result)
-                }
-            }
-        });
-
-        // Mutate other fields in Config
-        disabled_mir_passes(config);
-        release_mode(config);
-        no_codegen(config);
-        polonius(config, self.options.use_polonius);
+        setup_compiler(config, self.options, true);
     }
 
     /// The MIR is modified in place: borrow-checking requires the "promoted" MIR, which causes the
@@ -267,17 +265,6 @@ pub struct RunCompilerNormallyCallbacks<'a> {
 }
 impl<'a> Callbacks for RunCompilerNormallyCallbacks<'a> {
     fn config(&mut self, config: &mut Config) {
-        polonius(config, self.options.use_polonius);
-    }
-}
-impl<'a> RunCompilerNormallyCallbacks<'a> {
-    pub fn new(options: &'a CliOpts) -> Self {
-        Self { options }
-    }
-    /// Run rustc normally. `args` is the arguments passed to `rustc`'s command-line.
-    pub fn run_compiler(&mut self, args: Vec<String>) -> Result<(), ()> {
-        rustc_driver::RunCompiler::new(&args, self)
-            .run()
-            .map_err(|_| ())
+        setup_compiler(config, self.options, false);
     }
 }
