425741e0dd031607a0aa8acdaefae294ca3285f1
Nadrieril <nadrieril+git@gmail.com>
2025-04-17 15:50:11 +0200

    Use hax to resolve paths to def ids

--- charon/src/bin/charon-driver/translate/resolve_path.rs
+++ charon/src/bin/charon-driver/translate/resolve_path.rs
@@ -1,11 +1,12 @@
 //! Machinery to resolve a string path into a `DefId`. Based on `clippy_utils::def_path_res`.
+use std::sync::Arc;
+
+use hax_frontend_exporter::{self as hax, BaseState, SInto};
 use itertools::Itertools;
 use rustc_ast::Mutability;
-use rustc_hir::def::DefKind;
-use rustc_hir::def_id::{CrateNum, DefId, LocalDefId, LOCAL_CRATE};
-use rustc_hir::{ImplItemRef, ItemKind, Node, OwnerId, TraitItemRef};
+use rustc_hir::def_id::{CrateNum, DefId, LOCAL_CRATE};
 use rustc_middle::ty::{fast_reject::SimplifiedType, FloatTy, IntTy, TyCtxt, UintTy};
-use rustc_span::symbol::{Ident, Symbol};
+use rustc_span::symbol::Symbol;
 
 fn find_primitive_impls<'tcx>(
     tcx: TyCtxt<'tcx>,
@@ -43,73 +44,6 @@ fn find_primitive_impls<'tcx>(
     tcx.incoherent_impls(ty).iter().copied()
 }
 
-fn non_local_item_children_by_name(tcx: TyCtxt<'_>, def_id: DefId, name: Symbol) -> Vec<DefId> {
-    match tcx.def_kind(def_id) {
-        DefKind::Mod | DefKind::Enum | DefKind::Trait => tcx
-            .module_children(def_id)
-            .iter()
-            .filter(|child| child.ident.name == name)
-            .filter_map(|child| child.res.opt_def_id())
-            .collect(),
-        DefKind::Impl { .. } => tcx
-            .associated_item_def_ids(def_id)
-            .iter()
-            .copied()
-            .filter(|assoc_def_id| tcx.item_name(*assoc_def_id) == name)
-            .collect(),
-        _ => Vec::new(),
-    }
-}
-
-fn local_item_children_by_name(tcx: TyCtxt<'_>, local_id: LocalDefId, name: Symbol) -> Vec<DefId> {
-    let root_mod;
-    let item_kind = match tcx.hir_node_by_def_id(local_id) {
-        Node::Crate(r#mod) => {
-            root_mod = ItemKind::Mod(Ident::dummy(), r#mod);
-            &root_mod
-        }
-        Node::Item(item) => &item.kind,
-        _ => return Vec::new(),
-    };
-
-    let res = |ident: Ident, owner_id: OwnerId| {
-        if ident.name == name {
-            Some(owner_id.to_def_id())
-        } else {
-            None
-        }
-    };
-
-    match item_kind {
-        ItemKind::Mod(_, r#mod) => r#mod
-            .item_ids
-            .iter()
-            .filter_map(|&item_id| {
-                let ident = tcx.hir_item(item_id).kind.ident()?;
-                res(ident, item_id.owner_id)
-            })
-            .collect(),
-        ItemKind::Impl(r#impl) => r#impl
-            .items
-            .iter()
-            .filter_map(|&ImplItemRef { ident, id, .. }| res(ident, id.owner_id))
-            .collect(),
-        ItemKind::Trait(.., trait_item_refs) => trait_item_refs
-            .iter()
-            .filter_map(|&TraitItemRef { ident, id, .. }| res(ident, id.owner_id))
-            .collect(),
-        _ => Vec::new(),
-    }
-}
-
-fn item_children_by_name(tcx: TyCtxt<'_>, def_id: DefId, name: Symbol) -> Vec<DefId> {
-    if let Some(local_id) = def_id.as_local() {
-        local_item_children_by_name(tcx, local_id, name)
-    } else {
-        non_local_item_children_by_name(tcx, def_id, name)
-    }
-}
-
 /// Resolves a def path like `std::vec::Vec`.
 ///
 /// Can return multiple resolutions when there are multiple versions of the same crate, e.g.
@@ -122,14 +56,15 @@ fn item_children_by_name(tcx: TyCtxt<'_>, def_id: DefId, name: Symbol) -> Vec<De
 ///
 /// If the path does not correspond to an existing item, return the first subpath that doesn't
 /// correspond to an item.
-pub fn def_path_def_ids<'a>(
-    tcx: TyCtxt<'_>,
+pub fn def_path_def_ids<'a, 'tcx>(
+    s: &impl BaseState<'tcx>,
     path: &'a [&'a str],
 ) -> Result<Vec<DefId>, &'a [&'a str]> {
+    let tcx = s.base().tcx;
     let mut items = vec![];
     for (i, &segment_str) in path.iter().enumerate() {
-        let segment = Symbol::intern(segment_str);
         if i == 0 {
+            let segment = Symbol::intern(segment_str);
             items = tcx
                 .crates(())
                 .iter()
@@ -149,17 +84,11 @@ pub fn def_path_def_ids<'a>(
             items = items
                 .into_iter()
                 .flat_map(|def_id| {
-                    // When the current def_id is e.g. `struct S`, check the impl items in `impl S {
-                    // ... }`
-                    let inherent_impl_children = tcx
-                        .inherent_impls(def_id)
-                        .iter()
-                        .flat_map(|&impl_def_id| item_children_by_name(tcx, impl_def_id, segment));
-
-                    let direct_children = item_children_by_name(tcx, def_id, segment);
-
-                    inherent_impl_children.chain(direct_children)
+                    let hax_def: Arc<hax::FullDef> = def_id.sinto(s);
+                    hax_def.nameable_children(s)
                 })
+                .filter(|(child_name, _)| *child_name == segment_str)
+                .map(|(_, def_id)| def_id.to_rust_def_id())
                 .collect();
         }
         if items.is_empty() {
