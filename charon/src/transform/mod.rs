64244716a9a967811842f06070f0a25963a136ef
Sacha-Élie Ayoun <sachaayoun@gmail.com>
2025-05-07 16:23:16 +0200

    make ops_to_function_calls an LLBC pass
    Signed-off-by: Sacha-Élie Ayoun <sachaayoun@gmail.com>

--- charon/src/transform/mod.rs
+++ charon/src/transform/mod.rs
@@ -110,10 +110,6 @@ pub static ULLBC_PASSES: &[Pass] = &[
     // # Micro-pass: introduce intermediate assignments in preparation of the
     // [`index_to_function_calls`] pass.
     UnstructuredBody(&index_intermediate_assigns::Transform),
-    // # Micro-pass: replace the arrays/slices index operations with function
-    // calls.
-    // (introduces: ArrayIndexShared, ArrayIndexMut, etc.)
-    UnstructuredBody(&index_to_function_calls::Transform),
     // # Micro-pass: add the missing assignments to the return value.
     // When the function return type is unit, the generated MIR doesn't
     // set the return value to `()`. This can be a concern: in the case
@@ -132,6 +128,9 @@ pub static ULLBC_PASSES: &[Pass] = &[
 pub static LLBC_PASSES: &[Pass] = &[
     // # Go from ULLBC to LLBC (Low-Level Borrow Calculus) by reconstructing the control flow.
     NonBody(&ullbc_to_llbc::Transform),
+    // # Micro-pass: replace some unops/binops and the array aggregates with
+    // function calls (introduces: ArrayToSlice, etc.)
+    StructuredBody(&ops_to_function_calls::Transform),
     // # Micro-pass: Remove the discriminant reads (merge them with the switches)
     StructuredBody(&remove_read_discriminant::Transform),
     // Cleanup the cfg.
--- charon/src/transform/ops_to_function_calls.rs
+++ charon/src/transform/ops_to_function_calls.rs
@@ -2,10 +2,10 @@
 //! For instance, we desugar ArrayToSlice from an unop to a function call.
 //! This allows a more uniform treatment later on.
 //! TODO: actually transform all the unops and binops to function calls?
+use crate::llbc_ast::*;
 use crate::transform::TransformCtx;
-use crate::ullbc_ast::*;
 
-use super::ctx::UllbcPass;
+use super::ctx::LlbcPass;
 
 fn transform_st(s: &mut Statement) {
     match &s.content {
@@ -85,8 +85,8 @@ fn transform_st(s: &mut Statement) {
 }
 
 pub struct Transform;
-impl UllbcPass for Transform {
-    fn transform_body(&self, _ctx: &mut TransformCtx, b: &mut ExprBody) {
-        b.visit_statements(&mut transform_st);
+impl LlbcPass for Transform {
+    fn transform_body(&self, ctx: &mut TransformCtx, b: &mut ExprBody) {
+        b.body.visit_statements(&mut transform_st);
     }
 }
