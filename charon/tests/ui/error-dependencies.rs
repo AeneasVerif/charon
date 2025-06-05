4ddb51875b0584b5984f378286a593bdc5608db4
Nadrieril <nadrieril+git@gmail.com>
2025-06-05 14:43:47 +0200

    Support trait aliases

--- charon/tests/ui/error-dependencies.rs
+++ charon/tests/ui/error-dependencies.rs
@@ -1,19 +1,19 @@
 //@ known-failure
 //! This file tests the error messages that indicates why we attempt to translate a given
 //! definition.
-#![feature(ptr_metadata)]
+#![feature(pattern)]
 #![feature(register_tool)]
 #![register_tool(charon)]
 
 pub fn main() {
-    let _ = core::ptr::null::<u8>();
+    let _ = "".contains("");
     let _ = opaque::other_error();
 }
 
 #[charon::opaque]
 mod opaque {
     pub fn other_error() {
-        let _ = custom_null::<u8>();
+        let _ = takes_pattern::<&str>();
     }
-    fn custom_null<T: core::ptr::Thin>() {}
+    fn takes_pattern<T: std::str::pattern::Pattern>() {}
 }
