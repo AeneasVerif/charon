3ff32671c294e9e9db70799089304fe36879ae9d
Son Ho <hosonmarc@gmail.com>
2025-06-19 14:59:37 +0100

    Use the span of the impl rather than of the default method when duplicating default methods

--- charon/src/transform/duplicate_defaulted_methods.rs
+++ charon/src/transform/duplicate_defaulted_methods.rs
@@ -99,10 +99,14 @@ impl TransformPass for Transform {
                         is_global_initializer,
                         body,
                     } = fun_decl.clone();
+                    // We use the span of the *impl*, not the span of the
+                    // default method in the original trait declaration.
+                    let span = timpl.item_meta.span;
                     let item_meta = ItemMeta {
                         name: new_fun_name,
                         is_local: timpl.item_meta.is_local,
                         opacity,
+                        span,
                         ..item_meta
                     };
                     let signature = FunSig {
