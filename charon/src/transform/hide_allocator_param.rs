9893ed291d42186a9cf280bebf35410bdd8b4a55
Nadrieril <nadrieril+git@gmail.com>
2025-07-04 16:03:38 +0200

    Remove allocator param from more types

--- charon/src/transform/hide_allocator_param.rs
+++ charon/src/transform/hide_allocator_param.rs
@@ -8,14 +8,12 @@ use super::{ctx::TransformPass, TransformCtx};
 
 #[derive(Visitor)]
 struct RemoveLastParamVisitor {
-    types: HashSet<TypeDeclId>,
+    types: HashSet<TypeId>,
 }
 
 impl VisitAstMut for RemoveLastParamVisitor {
     fn enter_type_decl_ref(&mut self, x: &mut TypeDeclRef) {
-        if x.id.as_adt().is_some_and(|id| self.types.contains(id))
-            || x.id.as_builtin().is_some_and(|b| b.is_box())
-        {
+        if self.types.contains(&x.id) {
             // Remove the last param.
             x.generics.types.pop();
         }
@@ -28,25 +26,46 @@ impl TransformPass for Transform {
         if !ctx.options.hide_allocator {
             return;
         }
-        // TODO: add Vec, Rc, etc
-        let types = &["alloc::boxed::Box"];
+        let types = &[
+            "alloc::boxed::Box",
+            "alloc::vec::Vec",
+            "alloc::rc::Rc",
+            "alloc::sync::Arc",
+        ];
 
         let types: Vec<NamePattern> = types
             .into_iter()
             .map(|s| NamePattern::parse(s).unwrap())
             .collect_vec();
-        let types: HashSet<TypeDeclId> = ctx
+        let types: HashSet<TypeId> = ctx
             .translated
             .item_names
             .iter()
             .filter(|(_, name)| types.iter().any(|p| p.matches(&ctx.translated, name)))
             .filter_map(|(id, _)| id.as_type())
             .copied()
+            .map(TypeId::Adt)
+            .chain([TypeId::Builtin(BuiltinTy::Box)])
             .collect();
 
         for &id in &types {
-            if let Some(tdecl) = ctx.translated.type_decls.get_mut(id) {
-                tdecl.generics.types.pop();
+            if let Some(&id) = id.as_adt()
+                && let Some(tdecl) = ctx.translated.type_decls.get_mut(id)
+            {
+                struct SubstWithErrorVisitor(TypeVarId);
+                impl VarsVisitor for SubstWithErrorVisitor {
+                    fn visit_type_var(&mut self, v: TypeDbVar) -> Option<Ty> {
+                        if let DeBruijnVar::Bound(DeBruijnId::ZERO, var_id) = v
+                            && var_id == self.0
+                        {
+                            Some(TyKind::Error("removed allocator parameter".to_owned()).into_ty())
+                        } else {
+                            None
+                        }
+                    }
+                }
+                let tvar = tdecl.generics.types.pop().unwrap();
+                tdecl.visit_vars(&mut SubstWithErrorVisitor(tvar.index));
             }
         }
 
