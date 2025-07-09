ec9c107d0ea3135cdf020d8e23d1b539a97ac64c
ssyram <liguany126@126.com>
2025-07-09 17:32:42 +0800

    Update charon/src/ast/types.rs
    Co-authored-by: Nadrieril <Nadrieril@users.noreply.github.com>

--- charon/src/ast/types.rs
+++ charon/src/ast/types.rs
@@ -797,7 +797,9 @@ pub enum TyKind {
     /// variables (those that could appear in a function pointer type like `for<'a> fn(&'a u32)`),
     /// we need to bind them here.
     FnDef(RegionBinder<FnPtr>),
-    /// A placeholder for existential predicates -- the `Self` is erased in such predicates.
+    /// Fake type used in `dyn Trait` predicates: such a predicate has the `Self` type
+    /// existentially quantified. In order for generics to stay consistent, we use this placeholder to
+    /// represent the existentially quantified type.
     ExistentialPlaceholder,
     /// A type that could not be computed or was incorrect.
     #[drive(skip)]
