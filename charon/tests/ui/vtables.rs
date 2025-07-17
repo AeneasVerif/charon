7f2faccdb1703de4b4c3f2ed42a4b3aa3fffb129
ssyram <liguany126@126.com>
2025-07-17 11:05:43 +0800

    more examples

--- charon/tests/ui/vtables.rs
+++ charon/tests/ui/vtables.rs
@@ -14,7 +14,23 @@ impl Checkable<i32> for i32 {
         self.super_method(10) > 0
     }
 }
+trait Modifiable<T> {
+    fn modify(&mut self, arg: &T) -> T;
+}
+impl <T : Clone> Modifiable<T> for i32 {
+    fn modify(&mut self, arg: &T) -> T {
+        *self += 1;
+        arg.clone()
+    }
+}
+fn modify_trait_object<T : Clone>(arg: &T) -> T {
+    let x : &mut dyn Modifiable<T> = &mut 199;
+    x.modify(arg)
+}
 fn main() {
     let x : &dyn Checkable<i32> = &42;
-    x.check();
+    assert!(x.check());
+    let y : &mut dyn Modifiable<i32> = &mut 99;
+    assert!(!modify_trait_object(&"Hello".to_string()).is_empty());
+    assert_eq!(y.modify(&mut 100), 100);
 }
