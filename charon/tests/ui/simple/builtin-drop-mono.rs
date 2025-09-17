eb5b40f59b06d62aef7222fdda7e690a743f2205
Nadrieril <nadrieril+git@gmail.com>
2025-09-17 13:49:13 +0200

    Add tests

---
+++ charon/tests/ui/simple/builtin-drop-mono.rs
@@ -0,0 +1,8 @@
+//@ charon-args=--mir=elaborated
+//@ charon-args=--add-drop-bounds
+//@ charon-args=--monomorphize
+fn drop_array(_: [String; 4]) {}
+
+fn drop_slice(_: Box<[String]>) {}
+
+fn drop_tuple<A, B>(_: (A, B)) {}
---
+++ charon/tests/ui/simple/struct-with-drops.rs
@@ -0,0 +1,17 @@
+//@ charon-args=--mir=elaborated
+//@ charon-args=--add-drop-bounds
+struct A {
+    x: Box<u32>,
+    b: B,
+}
+
+struct B {
+    x: Box<u32>,
+    y: Box<u32>,
+}
+
+fn takes_a(_: A) {}
+
+impl Drop for B {
+    fn drop(&mut self) {}
+}
