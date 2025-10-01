a0a6b6fbab92779f5b4ee020754ab9038bfc8817
Nadrieril <nadrieril+git@gmail.com>
2025-10-01 15:50:09 +0200

    Just ask rustc whether types are sized

--- charon/src/transform/mod.rs
+++ charon/src/transform/mod.rs
@@ -33,7 +33,6 @@ pub mod remove_unused_locals;
 pub mod remove_unused_methods;
 pub mod remove_unused_self_clause;
 pub mod reorder_decls;
-pub mod resolve_sized_ptr_metadata_inherit;
 pub mod simplify_constants;
 pub mod skip_trait_refs_when_known;
 pub mod ullbc_to_llbc;
@@ -72,9 +71,6 @@ pub static INITIAL_CLEANUP_PASSES: &[Pass] = &[
     // directly instead of going via a `TraitRef`. This is done before `reorder_decls` to remove
     // some sources of mutual recursion.
     UnstructuredBody(&skip_trait_refs_when_known::Transform),
-    // For the `PtrMetadata::InheritFrom` case in the type definitions
-    // If the given inherited type var is bounded by `Sized`, we resolve it to `None`.
-    NonBody(&resolve_sized_ptr_metadata_inherit::Transform),
     // Compute the metadata & insert for Rvalue
     UnstructuredBody(&insert_ptr_metadata::Transform),
     // Change trait associated types to be type parameters instead. See the module for details.
