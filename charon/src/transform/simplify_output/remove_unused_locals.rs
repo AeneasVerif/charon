844956b960128ec4d3fe3f271a220f5b0e950d86
Nadrieril <nadrieril+git@gmail.com>
2025-10-02 09:57:51 +0200

    Improve perf of `remove_unused_locals`

--- charon/src/transform/simplify_output/remove_unused_locals.rs
+++ charon/src/transform/simplify_output/remove_unused_locals.rs
@@ -2,42 +2,41 @@
 //! never used in the function bodies.  This is useful to remove the locals with
 //! type `Never`. We actually check that there are no such local variables
 //! remaining afterwards.
-use std::collections::{HashMap, HashSet};
 use std::mem;
 
 use crate::ast::*;
 use crate::transform::TransformCtx;
-
 use crate::transform::ctx::TransformPass;
 
 fn remove_unused_locals<Body: BodyVisitable>(body: &mut GExprBody<Body>) {
     // Compute the set of used locals.
     // We always register the return variable and the input arguments.
-    let mut used_locals: HashSet<LocalId> = (0..(body.locals.arg_count + 1))
-        .map(|i| LocalId::new(i))
-        .collect();
+    let mut used_locals: Vector<LocalId, bool> = body
+        .locals
+        .locals
+        .map_ref(|local| local.index <= body.locals.arg_count);
     body.body.dyn_visit_in_body(|lid: &LocalId| {
-        used_locals.insert(*lid);
+        used_locals[*lid] = true;
     });
     trace!("used_locals: {:?}", used_locals);
 
     // Keep only the variables that are used and update their indices to be contiguous.
-    let mut ids_map: HashMap<LocalId, LocalId> = HashMap::new();
+    let mut ids_map: Vector<LocalId, Option<LocalId>> = body.locals.locals.map_ref(|_| None);
     for local in mem::take(&mut body.locals.locals) {
-        if used_locals.contains(&local.index) {
+        if used_locals[local.index] {
             let old_id = local.index;
             let new_id = body
                 .locals
                 .locals
                 .push_with(|index| Local { index, ..local });
-            ids_map.insert(old_id, new_id);
+            ids_map[old_id] = Some(new_id);
         }
     }
     trace!("ids_maps: {:?}", ids_map);
 
     // Update all `LocalId`s.
     body.body.dyn_visit_in_body_mut(|lid: &mut LocalId| {
-        *lid = *ids_map.get(lid).unwrap();
+        *lid = ids_map[*lid].unwrap();
     });
 }
 
