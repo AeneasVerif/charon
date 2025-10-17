e66a29302198fdb10c8cd296dfed830c638675a8
Nadrieril <nadrieril+git@gmail.com>
2025-10-17 15:15:47 +0200

    Find all infected types using a graph search

--- charon/src/transform/normalize/partial_monomorphization.rs
+++ charon/src/transform/normalize/partial_monomorphization.rs
@@ -275,23 +275,34 @@ struct PartialMonomorphizer<'a> {
 
 impl<'a> PartialMonomorphizer<'a> {
     pub fn new(ctx: &'a mut TransformCtx) -> Self {
+        use petgraph::graphmap::DiGraphMap;
+        use petgraph::visit::Dfs;
+        use petgraph::visit::Walker;
+
         // Compute the types that contain `&mut` (even indirectly).
-        // TODO: this needs a fixpoint. Might have to build a graph and do reachability.
-        let infected_types = ctx
-            .translated
-            .type_decls
-            .iter_indexed()
-            .filter(|(_, decl)| {
-                let mut any_ref_mut = false;
-                decl.dyn_visit(|x: &Ty| {
-                    if matches!(x.kind(), TyKind::Ref(_, _, RefKind::Mut)) {
-                        any_ref_mut = true;
+        let infected_types: HashSet<_> = {
+            // We build a graph that has one node per type decl plus a special `None` node. If type A
+            // contains a reference to type B we add a B->A edge; if it contains a mutable reference we
+            // add a None->A edge. Then a type contains `&mut` iff it is reachable from the `None`
+            // node.
+            let mut graph: DiGraphMap<Option<TypeDeclId>, ()> = Default::default();
+            for (id, tdecl) in ctx.translated.type_decls.iter_indexed() {
+                tdecl.dyn_visit(|x: &Ty| match x.kind() {
+                    TyKind::Ref(_, _, RefKind::Mut) => {
+                        graph.add_edge(None, Some(id), ());
                     }
+                    TyKind::Adt(tref) if let TypeId::Adt(other_id) = tref.id => {
+                        graph.add_edge(Some(other_id), Some(id), ());
+                    }
+                    _ => {}
                 });
-                any_ref_mut
-            })
-            .map(|(id, _)| id)
-            .collect();
+            }
+            let start = graph.add_node(None);
+            Dfs::new(&graph, start)
+                .iter(&graph)
+                .filter_map(|opt_id| opt_id)
+                .collect()
+        };
 
         // Record the generic params of all items.
         let generic_params: HashMap<ItemId, GenericParams> = ctx
