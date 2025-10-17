f3d3b00b5fc826bdc22870fd8ed60918e9122ac1
Nadrieril <nadrieril+git@gmail.com>
2025-10-17 14:18:12 +0200

    drive-by: simplify `HashConsed`

--- charon/src/common.rs
+++ charon/src/common.rs
@@ -110,10 +110,10 @@ pub mod type_map {
 pub mod hash_consing {
     use derive_generic_visitor::{Drive, DriveMut, Visit, VisitMut};
 
+    use super::hash_by_addr::HashByAddr;
     use super::type_map::{Mappable, Mapper, TypeMap};
-    use itertools::Either;
     use serde::{Deserialize, Serialize};
-    use std::collections::HashMap;
+    use std::collections::HashSet;
     use std::hash::Hash;
     use std::ops::ControlFlow;
     use std::sync::{Arc, LazyLock, RwLock};
@@ -121,12 +121,12 @@ pub mod hash_consing {
     /// Hash-consed data structure: a reference-counted wrapper that guarantees that two equal
     /// value will be stored at the same address. This makes it possible to use the pointer address
     /// as a hash value.
-    #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-    pub struct HashConsed<T>(Arc<T>);
+    #[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+    pub struct HashConsed<T>(HashByAddr<Arc<T>>);
 
     impl<T> HashConsed<T> {
         pub fn inner(&self) -> &T {
-            self.0.as_ref()
+            self.0.0.as_ref()
         }
     }
 
@@ -135,60 +135,60 @@ pub mod hash_consing {
         T: Hash + PartialEq + Eq + Clone + Mappable,
     {
         pub fn new(inner: T) -> Self {
-            Self::intern(Either::Left(inner))
+            Self::intern(inner)
         }
 
         /// Clones if needed to get mutable access to the inner value.
         pub fn with_inner_mut<R>(&mut self, f: impl FnOnce(&mut T) -> R) -> R {
-            let kind = Arc::make_mut(&mut self.0);
-            let ret = f(kind);
-            // Re-establish sharing, crucial for the hashing function to be correct.
-            *self = Self::intern(Either::Right(self.0.clone()));
+            // The value is behind a shared `Arc`, we clone it in order to mutate it.
+            let mut value = self.inner().clone();
+            let ret = f(&mut value);
+            // Re-intern the new value.
+            *self = Self::intern(value);
             ret
         }
 
-        /// Deduplicate the valuess by hashing them. This deduplication is crucial for the hashing
+        /// Deduplicate the values by hashing them. This deduplication is crucial for the hashing
         /// function to be correct. This is the only function allowed to create `Self` values.
-        fn intern(inner: Either<T, Arc<T>>) -> Self {
+        fn intern(inner: T) -> Self {
             struct InternMapper;
             impl Mapper for InternMapper {
-                type Value<T: Mappable> = HashMap<T, Arc<T>>;
+                type Value<T: Mappable> = HashSet<Arc<T>>;
             }
+            // This is a static mutable `HashSet<Arc<T>>` that records for each `T` value a unique
+            // `Arc<T>` that contains the same value. Values inside the set are hashed/compared
+            // as is normal for `T`.
+            // Once we've gotten an `Arc` out of the set however, we're sure that "T-equality"
+            // implies address-equality, hence the `HashByAddr` wrapper preserves correct equality
+            // and hashing behavior.
             static INTERNED: LazyLock<RwLock<TypeMap<InternMapper>>> =
                 LazyLock::new(|| Default::default());
 
             if INTERNED.read().unwrap().get::<T>().is_none() {
-                INTERNED.write().unwrap().insert::<T>(Default::default());
+                INTERNED.write().unwrap().insert::<T>(HashSet::default());
             }
             let read_guard = INTERNED.read().unwrap();
-            if let Some(inner) = (*read_guard)
-                .get::<T>()
-                .unwrap()
-                .get(inner.as_ref().either(|x| x, |x| x.as_ref()))
-            {
-                Self(inner.clone())
+            let arc = if let Some(arc) = (*read_guard).get::<T>().unwrap().get(&inner) {
+                arc.clone()
             } else {
                 drop(read_guard);
-                // We clone the value here in the slow path, which makes it possible to avoid an
-                // allocation in the fast path.
-                let raw_val: T = inner.as_ref().either(T::clone, |x| x.as_ref().clone());
-                let arc: Arc<T> = inner.either(Arc::new, |x| x);
+                let arc: Arc<T> = Arc::new(inner);
                 INTERNED
                     .write()
                     .unwrap()
                     .get_mut::<T>()
                     .unwrap()
-                    .insert(raw_val, arc.clone());
-                Self(arc)
-            }
+                    .insert(arc.clone());
+                arc
+            };
+            Self(HashByAddr(arc))
         }
     }
 
-    /// Hash the pointer; this is only correct if two identical values of `Self` are guaranteed to
-    /// point to the same memory location, which we carefully enforce above.
-    impl<T> std::hash::Hash for HashConsed<T> {
-        fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-            Arc::as_ptr(&self.0).hash(state);
+    impl<T: std::fmt::Debug> std::fmt::Debug for HashConsed<T> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            // Hide the `HashByAddr` wrapper.
+            f.debug_tuple("HashConsed").field(self.inner()).finish()
         }
     }
 
@@ -210,6 +210,7 @@ pub mod hash_consing {
 }
 
 pub mod hash_by_addr {
+    use serde::{Deserialize, Serialize};
     use std::{
         hash::{Hash, Hasher},
         ops::Deref,
@@ -217,7 +218,7 @@ pub mod hash_by_addr {
 
     /// A wrapper around a smart pointer that hashes and compares the contents by the address of
     /// the pointee.
-    #[derive(Debug, Clone)]
+    #[derive(Debug, Clone, Serialize, Deserialize)]
     pub struct HashByAddr<T>(pub T);
 
     impl<T: Deref> HashByAddr<T> {
