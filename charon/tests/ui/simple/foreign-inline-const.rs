e061f5d2b8eb6cd75cc22d7832f7364526e4ace4
Nadrieril <nadrieril+git@gmail.com>
2025-11-24 17:43:56 +0100

    Add tests

---
+++ charon/tests/ui/simple/foreign-inline-const.rs
@@ -0,0 +1,6 @@
+//@ known-failure
+//@ aux-crate=closure-inside-inline-const.rs
+//@ charon-args=--include=closure_inside_inline_const
+fn bar<T>() {
+    closure_inside_inline_const::foo::<T>();
+}
