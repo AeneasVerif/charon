63e54479d5616e9beb123e307c9ca3572fb24db5
Nadrieril <nadrieril+git@gmail.com>
2026-03-30 16:16:55 +0200

    Add tests

---
+++ charon/tests/ui/associated_types/assoc-ty-default-with-self-bound.rs
@@ -0,0 +1,11 @@
+//@ known-failure
+#![feature(associated_type_defaults)]
+pub trait MyTrait {
+    type Ret: Sized
+        = ()
+    where
+        Self: Sized;
+}
+
+pub struct S;
+impl MyTrait for S {}
---
+++ charon/tests/ui/simple/defaulted-rpitit.3.rs
@@ -0,0 +1,8 @@
+//@ known-failure
+pub trait MyTrait<T> {
+    fn rows(&self) -> impl Iterator<Item = T> {
+        core::iter::empty::<T>()
+    }
+}
+
+impl<T> MyTrait<T> for () {}
