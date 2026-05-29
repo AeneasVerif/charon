102be628f20f25c576f14085d6e50b66fcf95602
Son Ho <hosonmarc@gmail.com>
2026-05-29 15:27:49 +0100

    Deactivate --monomorphize-mut=except-types for --presets=aeneas

--- charon/src/options.rs
+++ charon/src/options.rs
@@ -423,7 +423,6 @@ impl CliOpts {
                     self.hide_allocator = true;
                     self.remove_unused_self_clauses = true;
                     self.remove_adt_clauses = true;
-                    self.monomorphize_mut = Some(MonomorphizeMut::ExceptTypes);
                     self.unbind_item_vars = true;
                 }
                 Preset::Eurydice => {
