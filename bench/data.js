ad6b11c76e44e82294281aee58130b4c8bc129aa
github-action-benchmark <github@users.noreply.github.com>
2026-06-02 01:36:16 +0200

    add Benchmark (customSmallerIsBetter) benchmark result for 12bba4cdc634d54c059e5b95f4854c69e356d942

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780355417674,
+  "lastUpdate": 1780356976678,
   "repoUrl": "https://github.com/AeneasVerif/charon",
   "entries": {
     "Benchmark": [
@@ -4562,6 +4562,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "libsignal-crypto",
             "value": 585.7,
+            "unit": "max-rss(MB)"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 5,
+            "unit": "output-size(MB)"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Nadrieril@users.noreply.github.com",
+            "name": "Nadrieril",
+            "username": "Nadrieril"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "12bba4cdc634d54c059e5b95f4854c69e356d942",
+          "message": "Merge pull request #1239 from Nadrieril/pprint-constant-types\n\nTweak the printing of constant literals",
+          "timestamp": "2026-06-01T23:32:21Z",
+          "tree_id": "23d0d0524c2c8870ed708cb579a61edb02bc180a",
+          "url": "https://github.com/AeneasVerif/charon/commit/12bba4cdc634d54c059e5b95f4854c69e356d942"
+        },
+        "date": 1780356975593,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 102205638679,
+            "unit": "instructions"
+          },
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 15.24,
+            "unit": "wall-clock(s)"
+          },
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 440.3,
+            "unit": "max-rss(MB)"
+          },
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 24.6,
+            "unit": "output-size(MB)"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 355018513091,
+            "unit": "instructions"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 45.66,
+            "unit": "wall-clock(s)"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 586.1,
             "unit": "max-rss(MB)"
           },
           {
