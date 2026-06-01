a24898db6edf1b79dc5f2a49e385e6fa3cd1e2d5
github-action-benchmark <github@users.noreply.github.com>
2026-06-01 22:39:35 +0200

    add Benchmark (customSmallerIsBetter) benchmark result for f76ec9477b62f01f06437b0b3174ea0675034eb6

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780320509212,
+  "lastUpdate": 1780346375205,
   "repoUrl": "https://github.com/AeneasVerif/charon",
   "entries": {
     "Benchmark": [
@@ -4365,6 +4365,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "libsignal-crypto",
             "value": 42.05,
+            "unit": "wall-clock(s)"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 586.3,
+            "unit": "max-rss(MB)"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 5,
+            "unit": "output-size(MB)"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Nadrieril@users.noreply.github.com",
+            "name": "Nadrieril",
+            "username": "Nadrieril"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f76ec9477b62f01f06437b0b3174ea0675034eb6",
+          "message": "Merge pull request #1230 from Nadrieril/typeid2\n\nSupport `TypeId`",
+          "timestamp": "2026-06-01T20:35:53Z",
+          "tree_id": "62d2c0492d7cb99ce156adc827250e8dfb6fabfa",
+          "url": "https://github.com/AeneasVerif/charon/commit/f76ec9477b62f01f06437b0b3174ea0675034eb6"
+        },
+        "date": 1780346373773,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 101722385605,
+            "unit": "instructions"
+          },
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 14.39,
+            "unit": "wall-clock(s)"
+          },
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 446.3,
+            "unit": "max-rss(MB)"
+          },
+          {
+            "name": "SparsePostQuantumRatchet",
+            "value": 24.6,
+            "unit": "output-size(MB)"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 352428405286,
+            "unit": "instructions"
+          },
+          {
+            "name": "libsignal-crypto",
+            "value": 41.05,
             "unit": "wall-clock(s)"
           },
           {
