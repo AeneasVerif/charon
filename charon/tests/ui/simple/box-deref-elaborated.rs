76ca9dc7cdbc087e65162ff030697953ab57af2c
Nadrieril <nadrieril+git@gmail.com>
2026-06-03 22:53:58 +0200

    Add tests

---
+++ charon/tests/ui/simple/box-deref-elaborated.rs
@@ -0,0 +1,25 @@
+//@ charon-args=--mir elaborated
+fn into_inner(b: Box<String>) -> String {
+    *b
+}
+
+fn write(mut b: Box<String>, s: String) {
+    *b = s;
+}
+
+fn borrow(mut b: Box<String>) {
+    let _ = &mut *b;
+}
+
+fn reborrow(b: &mut Box<String>) -> &mut String {
+    &mut *b
+}
+
+struct Struct {
+    a: i32,
+    b: String,
+}
+
+fn field(b: Box<Struct>) -> String {
+    b.b
+}
