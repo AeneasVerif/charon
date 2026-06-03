bf6f454a050ccbfe017f47e2c632895ea90eb91d
Nadrieril <nadrieril+git@gmail.com>
2026-06-03 18:21:51 +0200

    Tweak test

--- charon/tests/ui/raw-boxes.rs
+++ charon/tests/ui/raw-boxes.rs
@@ -7,6 +7,7 @@ unsafe fn foo() {
     let b = Box::new(42);
     let p = Box::into_raw(b);
     let _ = Box::leak(Box::new(42));
-    let b = Box::from_raw(p);
+    let mut b = Box::from_raw(p);
     let i = *b;
+    *b = 42;
 }
