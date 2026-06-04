9147f57b4dcc08271c8c38f3740bd1d2fd047bfc
Derek Sorensen <d@dhsorens.com>
2026-06-04 13:21:04 +0100

    fix: apply suggestion from @Nadrieril
    Co-authored-by: Nadrieril <Nadrieril@users.noreply.github.com>

--- charon/tests/ui/associated_types/issue-1260-self-referential-assoc-ty.rs
+++ charon/tests/ui/associated_types/issue-1260-self-referential-assoc-ty.rs
@@ -3,13 +3,12 @@
 #![no_std]
 
 pub trait Ring {
-    type PrimeSubfield: PrimeField;
+    type PrimeSubfield: Field;
     const ZERO: Self;
 }
 pub trait Field: Ring {
     type Packing;
 }
-pub trait PrimeField: Field {}
 
 pub fn sub_field_zero<R: Ring>() -> R::PrimeSubfield {
     R::PrimeSubfield::ZERO
