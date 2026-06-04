47f8359ed543314b236146d3d6f299d47384667e
Nadrieril <nadrieril+git@gmail.com>
2026-06-04 13:59:10 +0200

    Add test

---
+++ charon/tests/ui/associated_types/issue-1260-self-ref-assoc-const.rs
@@ -0,0 +1,15 @@
+//@ known-failure
+//@ charon-args=--lift-associated-types=*
+
+trait Ring {
+    type Sub: Field;
+    const ZERO: Self;
+}
+
+trait Field: Ring {
+    type Packing;
+}
+
+fn zero<R: Ring>() {
+    let _ = R::Sub::ZERO;
+}
