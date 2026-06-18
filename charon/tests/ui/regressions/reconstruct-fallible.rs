8288eaed99ec8a0f549577f9a5e889a6f27f34db
N1ark <opale.sjostedt@gmail.com>
2026-06-18 17:01:53 +0100

    Add failing test

---
+++ charon/tests/ui/regressions/reconstruct-fallible.rs
@@ -0,0 +1,41 @@
+//@ charon-args=--sysroot default --monomorphize --mir elaborated
+
+#![feature(core_intrinsics)]
+
+fn other() {}
+fn sink_tuple(_x: (usize, bool)) {}
+fn sink_int(_x: usize) {}
+fn sink_bool(_x: bool) {}
+
+fn must_preserve_tuple() {
+    let z = std::intrinsics::add_with_overflow(0usize, 0usize);
+    other();
+    sink_tuple(z);
+}
+
+fn can_erase_check() {
+    let z = std::intrinsics::add_with_overflow(0usize, 0usize);
+    other();
+    sink_int(z.0);
+}
+
+fn must_preserve_overflow_flag() {
+    let z = std::intrinsics::add_with_overflow(0usize, 0usize);
+    other();
+    sink_bool(z.1);
+}
+
+fn mixed_field0_and_cross_block_tuple() {
+    let z = std::intrinsics::add_with_overflow(0usize, 0usize);
+    sink_int(z.0);
+    other();
+    sink_tuple(z);
+}
+
+fn can_erase_check_multi_block() {
+    let z = std::intrinsics::add_with_overflow(0usize, 0usize);
+    other();
+    sink_int(z.0);
+    other();
+    sink_int(z.0);
+}
