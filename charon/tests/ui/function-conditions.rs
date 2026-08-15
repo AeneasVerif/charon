cbdc4a71c1ef1c2b8a5b1519cc30a5242e8ad3da
Mate Kovacs <mkovaxx@gmail.com>
2026-08-15 19:29:16 +0900

    update test

--- charon/tests/ui/function-conditions.rs
+++ charon/tests/ui/function-conditions.rs
@@ -1,7 +1,14 @@
 //@ charon-args=--start-from=crate::function
-fn function() {
-    #[charon::precondition]
-    fn precondition() -> bool {
-        true
-    }
+fn function(input: i32) -> i32 {
+    input + input
+}
+
+#[charon::precondition(name = "function")]
+fn precondition(input: i32) -> bool {
+    input != 0
+}
+
+#[charon::postcondition(name = "function")]
+fn postcondition(input: i32, output: i32) -> bool {
+    output == input + 42
 }
