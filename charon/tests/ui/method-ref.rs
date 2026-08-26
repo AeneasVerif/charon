346e086e9ed2bed4a1b51846fb7550a6e47743b3
Nadrieril <nadrieril+git@gmail.com>
2026-08-26 18:35:41 +0200

    Add tests

--- charon/tests/ui/method-ref.rs
+++ charon/tests/ui/method-ref.rs
@@ -2,6 +2,9 @@ trait Ord {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering;
 }
 
-fn min<T: Ord>() {
+// fn use_fn<T, F: FnOnce(&T, &T) -> std::cmp::Ordering>(_: F) {}
+
+fn method_ref<T: Ord>() {
     let _ = T::cmp;
+    // use_fn::<T, _>(T::cmp);
 }
