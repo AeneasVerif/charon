2f6cf4c380863d6a0f9637e92de738d45bacb299
Nadrieril <nadrieril+git@gmail.com>
2026-09-08 21:01:14 +0200

    Add test

---
+++ charon/tests/ui/traits/issue-1403-default-method-with-item-constraint.rs
@@ -0,0 +1,20 @@
+//@ known-failure
+//@ no-default-options
+//@ charon-args=--translate-all-methods
+//@ charon-args=--lift-associated-types=*
+pub struct A;
+pub struct B;
+
+pub trait Trait {
+    type Assoc;
+
+    fn method(&self)
+    where
+        Self: Trait<Assoc = B>,
+    {
+    }
+}
+
+impl Trait for () {
+    type Assoc = A;
+}
