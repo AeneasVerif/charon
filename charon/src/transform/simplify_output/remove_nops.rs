d56568e3667999eec858c83694e96c243f6e7728
Nadrieril <nadrieril+git@gmail.com>
2026-09-09 18:40:16 +0200

    Don't discard `Nop`s if they have comments
    This makes for a cute way to annotate the output,
    as only the comments will be displayed. Also solves the issue of
    end-of-block comments.

--- charon/src/transform/simplify_output/remove_nops.rs
+++ charon/src/transform/simplify_output/remove_nops.rs
@@ -2,30 +2,36 @@
 use crate::ast::*;
 use crate::transform::TransformCtx;
 
-use crate::transform::ctx::TransformPass;
+use crate::transform::ctx::{LlbcPass, TransformPass, UllbcPass};
 
 pub struct Transform;
+impl UllbcPass for Transform {
+    fn transform_body(&self, _ctx: &mut TransformCtx, body: &mut ullbc_ast::ExprBody) {
+        for blk in &mut body.body {
+            blk.statements
+                .retain(|st| !(st.kind.is_nop() && st.comments_before.is_empty()))
+        }
+    }
+}
+impl LlbcPass for Transform {
+    fn transform_body(&self, _ctx: &mut TransformCtx, body: &mut llbc_ast::ExprBody) {
+        body.body.visit_blocks_bwd(|blk: &mut llbc_ast::Block| {
+            blk.statements
+                .retain(|st| !(st.kind.is_nop() && st.comments_before.is_empty()))
+        });
+    }
+}
+
 impl TransformPass for Transform {
     fn transform_ctx(&self, ctx: &mut TransformCtx) {
-        ctx.for_each_fun_decl(|_ctx, fun| {
-            match &mut fun.body {
-                Body::Unstructured(body) => {
-                    for blk in &mut body.body {
-                        if blk.statements.iter().any(|st| st.kind.is_nop()) {
-                            blk.statements.retain(|st| !st.kind.is_nop())
-                        }
-                    }
-                }
-                Body::Structured(body) => {
-                    body.body.visit_blocks_bwd(|blk: &mut llbc_ast::Block| {
-                        // Remove all the `Nop`s from this sequence.
-                        if blk.statements.iter().any(|st| st.kind.is_nop()) {
-                            blk.statements.retain(|st| !st.kind.is_nop())
-                        }
-                    });
-                }
-                _ => {}
+        ctx.for_each_fun_decl(|ctx, fun| match &mut fun.body {
+            Body::Unstructured(body) => {
+                <Self as UllbcPass>::transform_body(self, ctx, body);
             }
+            Body::Structured(body) => {
+                <Self as LlbcPass>::transform_body(self, ctx, body);
+            }
+            _ => {}
         });
     }
 }
